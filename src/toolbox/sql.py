0e71232982bc04602d70e9cb764e713917a819bc
elad.noor <elad.noor@48c8c3c8-1ab2-6309-a68e-2c090a688e41>
2010-10-26 22:46:03 +0000

    Added a printout of the reasons for not having a dG0 estimation for some compounds, when using group contribution.

--- src/toolbox/sql.py
+++ src/toolbox/sql.py
@@ -7,11 +7,37 @@ def write_csv_query(cursor, filename, query, titles=None):
     for row in cursor.execute(query):
         writer.writerow(list(row))
 
-def write_csv_table(cursor, filename, tablename, titles=True):
+def write_csv_table(cursor, filename, tablename, write_titles=True):
+    column_names = []
+    for row in cursor.execute("PRAGMA table_info(%s)" % tablename):
+        #(index, name, type, can_be_null, default_value, stam) = row
+        column_names.append(row[1])
+    
     writer = csv.writer(open(filename, 'w'))
-    cursor.execute("SELECT * FROM " + tablename)
-    if (titles):
-        col_name_list = [tuple[0] for tuple in cursor.description]
-        writer.writerow(col_name_list)
+    if (write_titles):
+        writer.writerow(column_names)
+
+    cursor.execute("SELECT * FROM %s" % tablename)
     for row in cursor:
         writer.writerow(list(row))
+
+def write_table_to_html(cursor, html_writer, tablename):
+    column_names = []
+    for row in cursor.execute("PRAGMA table_info(%s)" % tablename):
+        #(index, name, type, can_be_null, default_value, stam) = row
+        column_names.append(row[1])
+    
+    html_writer.write('<table border="1">\n')
+    html_writer.write('  <tr>' + "".join([('<td><b>' + str(s) + '</b></td>') for s in column_names]) + '</tr>\n')
+    for row in cursor.execute("SELECT * FROM %s" % tablename):
+        html_writer.write('  <tr>' + "".join([('<td>' + str(s) + '</td>') for s in row]) + '</tr>\n')
+    html_writer.write('</table>\n')
+
+def write_query_to_html(cursor, html_writer, query, column_names=None):
+    html_writer.write('<table border="1">\n')
+    if (column_names != None):
+        html_writer.write('  <tr>' + "".join([('<td><b>' + str(s) + '</b></td>') for s in column_names]) + '</tr>\n')
+    for row in cursor.execute(query):
+        html_writer.write('  <tr>' + "".join([('<td>' + str(s) + '</td>') for s in row]) + '</tr>\n')
+    html_writer.write('</table>\n')
+    html_writer.write('</p>\n')
