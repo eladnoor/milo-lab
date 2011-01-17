c60a57640aa770e631b9068d1d715800a332f7b6
flamholz <flamholz@48c8c3c8-1ab2-6309-a68e-2c090a688e41>
2011-01-17 16:38:14 +0000

    Show Keq and untransformed DGs

--- src/gibbs_site/gibbs/search_results_page.py
+++ src/gibbs_site/gibbs/search_results_page.py
@@ -19,14 +19,12 @@ def ResultsPage(request):
     matcher = service_config.Get().compound_matcher
     
     query = form.cleaned_query
-    logging.info(query)
-    logging.info('Is reaction query: %s' % query_parser.IsReactionQuery(query))
     ph = form.cleaned_ph
     ionic_strength = form.cleaned_ionic_strength
     template_data = {'query': query,
                      'ph': ph,
                      'ionic_strength': ionic_strength}
-    
+        
     # Check if we should parse and process the input as a reaction.
     if query_parser.IsReactionQuery(query):
         try:
@@ -42,19 +40,14 @@ def ResultsPage(request):
             raise Http404
         
         reactants, products = best_reaction
-        
         cprofile = concentration_profile.GetProfile()
-        rxn = reaction.Reaction.FromIds(reactants, products, cprofile)
-        delta_g_estimate = rxn.DeltaG(
-            pH=ph, ionic_strength=ionic_strength)
-
-        balance_with_water_link = rxn.GetBalanceWithWaterLink(
-            ph, ionic_strength, cprofile.name, query)
-        balance_electrons_link = rxn.GetBalanceElectronsLink(
-            ph, ionic_strength, cprofile.name, query)
-        template_data.update({'delta_g_estimate': delta_g_estimate,
-                              'no_dg_explanation': rxn.NoDeltaGExplanation(),
-                              'reaction': rxn,
+        rxn = reaction.Reaction.FromIds(reactants, products,
+                                        concentration_profile=cprofile,
+                                        pH=ph, ionic_strength=ionic_strength)
+        
+        balance_with_water_link = rxn.GetBalanceWithWaterLink(query)
+        balance_electrons_link = rxn.GetBalanceElectronsLink(query)
+        template_data.update({'reaction': rxn,
                               'balance_with_water_link': balance_with_water_link,
                               'balance_electrons_link': balance_electrons_link})
         return render_to_response('reaction_page.html', template_data)
