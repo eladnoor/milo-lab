4d778418c42b2598a520cc58cdea57e4738365b3
elad.noor <elad.noor@48c8c3c8-1ab2-6309-a68e-2c090a688e41>
2011-02-06 20:46:05 +0000

    Tecan parsing - work in progress...

--- trunk/src/pytecan/2011-02-06_PL6-96.py
+++ trunk/src/pytecan/2011-02-06_PL6-96.py
@@ -3,31 +3,28 @@ from pylab import *
 from matplotlib import font_manager
 from matplotlib.backends.backend_pdf import PdfPages
 from pytecan.ReaderXML import CollectData
+from toolbox.util import _mkdir
 
-def get_data(index, row, col, MES):
+def get_data(reading_label, plate_id, row, col, MES):
     """
         When the experimental data is broken into more than one XLS sheet, this method
         concatenates the data into one series and returns it as if it was from one source.
     """
-    time_array = array([])
-    value_array = array([])
-    last_t = 0
-    (time, value) = vp.get_data(index, row, col)
-    time_array = hstack([time_array, time + last_t])
-    value_array = hstack([value_array, value])
-    if (len(time_array) > 0):
-        last_t = time_array.max()
+    well = (row, col)
+    time_list = []
+    value_list = []
+    for time, value in sorted(MES[reading_label][plate_id][well].iteritems()):
+         time_list.append(time)
+         value_list.append(value)
+         
+    time_array = array(time_list)
+    if len(time_list):
+        time_array = (time_array - time_list[0])/3600 
+    return time_array, array(value_list)
 
-    return (time_array, value_array)
-
-if (not os.path.exists('../res')):
-    os.mkdir('../res')
-if (not os.path.exists('../res/victor')):
-    os.mkdir('../res/victor')
-
-MES = CollectData(dir="../data/tecan/PL6-96", number_of_plates=4)
-
-pp = PdfPages('../res/victor/2011-01-11_serine_growth.pdf')
+MES = CollectData("../data/tecan/PL6-96.tar.gz", number_of_plates=4)
+_mkdir('../res/tecan')
+pp = PdfPages('../res/tecan/2011-01-11_serine_growth.pdf')
 
 #rcParams['text.usetex'] = True
 rcParams['legend.fontsize'] = 12
@@ -39,21 +36,22 @@ rcParams['legend.fontsize'] = 12
 #rcParams['figure.subplot.hspace'] = 0.3
 #figure()
 
-plot_growth_rate = True
+plot_growth_rate = False
 fit_window_size = 1.5 # hours
 fit_start_threshold = 0.01
 
 plots = [] # (title, victor_index, (t_min, t_max), (y_min, y_max), y_label, 
-t_max = 65
-OD_min = 0.046
+t_max = 42
+OD_min = 0.036
 
 #for r in [0, 1, 2, 3, 4, 5, 6, 7]:
 colors = ['gray', 'red', 'magenta', 'blue', 'cyan', 'green', 'pink', 'orange', 'black', 'r:', 'g:', 'b:', 'c:']
-for r in [0, 2]:
+plate_id = 0
+for r in [0, 1, 2, 3, 4, 5]:
     vlegend = []
-    for c in xrange(12):
-        vlegend += [('%s%d' % (chr(ord('A') + r), c+1), colors[c], [(r, c)])]
-    plots.append(('Serine Growth row %s' % chr(ord('A') + r), (0, t_max), (1e-3, 3e-1), 'OD', vlegend))
+    for c in [0, 1, 2, 3, 4, 5, 6, 7]:
+        vlegend += [('%s%d' % (chr(ord('A') + r), c+1), colors[c], [(plate_id, r, c)])]
+    plots.append(('Serine Growth row %s' % chr(ord('A') + r), (0, t_max), (1e-3, 3e-1), 'OD600', vlegend))
 
 for (plot_title, t_range, y_range, y_label, data_series) in plots:
     sys.stderr.write("Plotting %s (%s) ... \n" % (plot_title, y_label))
@@ -65,9 +63,10 @@ for (plot_title, t_range, y_range, y_label, data_series) in plots:
     label2legend = {}
     label2line = []
     for (label, color, cells) in data_series:
-        for (row, col) in cells:
-            (time0, values0) = get_data(y_label, row, col, MES)
-            
+        for (plate_id, row, col) in cells:
+            (time, values) = get_data(y_label, plate_id, row, col, MES)
+            if OD_min:
+                values -= OD_min
             line = plot(time, values, color)
             if (label not in label2legend):
                 label2line.append((line, label))
