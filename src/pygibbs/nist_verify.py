cc8b6534ec922ca8b8b9a25ebb6cee4a2230fe09
elad.noor@gmail.com <elad.noor@gmail.com@48c8c3c8-1ab2-6309-a68e-2c090a688e41>
2011-03-01 10:50:24 +0000

    checkin for elad

--- src/pygibbs/nist_verify.py
+++ src/pygibbs/nist_verify.py
@@ -26,6 +26,9 @@ def main():
     html_writer = HtmlWriter("../res/nist/report.html")
     nist = Nist(db, html_writer)
     nist.Load()
+    nist.T_range = (298, 314)
+    #nist.override_I = 0.25
+    nist.override_pMg = 10.0
 
     estimators = {}
     estimators['Alberty'] = PsuedoisomerTableThermodynamics.FromDatabase(
@@ -37,7 +40,6 @@ def main():
     estimators['NIST regression'] = regress
         
     gc = GroupContribution(db, html_writer)
-    gc.override_gc_with_measurements = True
     gc.init()
     estimators['Milo Group Contribution'] = gc
     
@@ -46,7 +48,7 @@ def main():
         html_writer.write('<p><b>%s </b>\n' % key)
         html_writer.insert_toggle(key)
         html_writer.start_div(key)
-        num_estimations, rmse = nist.verify_results(thermodynamics, T_range=(298, 314))
+        num_estimations, rmse = nist.verify_results(thermodynamics)
         html_writer.end_div()
         html_writer.write('N = %d, RMSE = %.1f</p>\n' % (num_estimations, rmse))
         logging.info('N = %d, RMSE = %.1f' % (num_estimations, rmse))
