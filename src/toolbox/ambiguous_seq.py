3390e80a706ed61a92c1b1066e70511c831895ac
flamholz <flamholz@48c8c3c8-1ab2-6309-a68e-2c090a688e41>
2011-03-23 08:29:08 +0000

    Class and test for representing ambiguous DNA sequences for the RBS project. Allows iteration over all concrete sequences without explicitly storing any individual sequence (avoiding memory waste for large combinatorics).

---
+++ src/toolbox/ambiguous_seq.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python
+
+import itertools
+
+from Bio.Alphabet import DNAAlphabet
+from Bio.Alphabet.IUPAC import IUPACAmbiguousDNA
+from Bio.Seq import Seq
+
+
+class AmbigousDNASeq(Seq):
+    """Class containing ambiguous DNA sequences."""
+    
+    AMBIGUOUS_BASE_MAP = {'R': ('G', 'A'),  # Purines
+                          'Y': ('T', 'C'),  # Pyrimidine
+                          'K': ('G', 'T'),  # Keto
+                          'M': ('A', 'C'),  # Amino
+                          'S': ('G', 'C'),
+                          'W': ('A', 'T'),
+                          'B': ('G', 'T', 'C'),
+                          'D': ('G', 'A', 'T'),
+                          'H': ('A', 'C', 'T'),
+                          'V': ('G', 'C', 'A'),
+                          'N': ('A', 'G', 'C', 'T'),
+                          }
+    
+    def __init__(self, seq_str):
+        Seq.__init__(self, seq_str, IUPACAmbiguousDNA())
+        
+        self._seq_list = list(self.tostring())
+    
+    def AmbiguousPositions(self):
+        """Return which positions have ambiguous bases."""
+        return [i for i, base in enumerate(self._seq_list)
+                if base in self.AMBIGUOUS_BASE_MAP]
+        
+    def IsConcrete(self):
+        """Returns True if this sequence is not ambiguous."""
+        return not self.AmbiguousPositions()
+    
+    def GetConcreteSeq(self):
+        """Returns a Seq object if this is a concrete sequence."""
+        if not self.IsConcrete():
+            raise TypeError('This sequence %s is not concrete' % self.tostring())
+        
+        return Seq(self.tostring(), DNAAlphabet())
+    
+    def AllConcreteSeqs(self):
+        """An iterator over all the concrete sequences denoted by this
+           ambiguous one.
+        
+        Yields:
+            Each possible concrete sequence as a Bio.Seq.Seq object.
+        """
+        ambiguous_positions = self.AmbiguousPositions()
+        
+        ambiguous_position_ranges = []
+        for pos in ambiguous_positions:
+            ambig_base = self._seq_list[pos]
+            substitutions = self.AMBIGUOUS_BASE_MAP[ambig_base]
+            r = range(len(substitutions))
+            ambiguous_position_ranges.append(range(len(substitutions)))
+        
+        for concrete_indices in itertools.product(*ambiguous_position_ranges):
+            new_seq_list = list(self._seq_list)
+            
+            for ambig_index, concrete_index in enumerate(concrete_indices):
+                pos = ambiguous_positions[ambig_index]
+                ambig_base = self._seq_list[pos]
+                substitution = self.AMBIGUOUS_BASE_MAP[ambig_base][concrete_index]
+                
+                new_seq_list[pos] = substitution
+            
+            yield Seq(''.join(new_seq_list), DNAAlphabet())
+                
+                
---
+++ src/toolbox/ambiguous_seq_test.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+import unittest
+
+from toolbox import ambiguous_seq
+
+
+class TestAmbigousDNASeq(unittest.TestCase):
+    
+    def assertSeqsEqual(self, expected_seqs, actual_seqs):
+        expected_set = set([str(s) for s in expected_seqs])
+        actual_set = set([str(s) for s in actual_seqs])
+        self.assertEqual(expected_set, actual_set)
+    
+    def testAmbiguousPositions(self):
+        
+        seq = ambiguous_seq.AmbigousDNASeq('ACTGGGA')
+        self.assertEqual([], seq.AmbiguousPositions())
+        self.assertTrue(seq.IsConcrete())
+        self.assertEqual('ACTGGGA', seq.GetConcreteSeq().tostring())
+        
+        seq = ambiguous_seq.AmbigousDNASeq('ACNGGVA')
+        self.assertEqual([2,5], seq.AmbiguousPositions())
+        
+        seq = ambiguous_seq.AmbigousDNASeq('ACNYGVACGTNNND')
+        self.assertEqual([2, 3, 5, 10, 11, 12, 13], seq.AmbiguousPositions())
+    
+    def testAllConcreteSeqsSimple(self):
+        ambig_seq = ambiguous_seq.AmbigousDNASeq('ACTGGGA')
+        concrete_seq = ambig_seq.GetConcreteSeq()
+        self.assertSeqsEqual([concrete_seq], ambig_seq.AllConcreteSeqs())
+        
+    def testAllConcreteSeqsWithAmbigBases(self):
+        ambig_seq = ambiguous_seq.AmbigousDNASeq('ACNGGVA')
+        expected_seqs = ['ACAGGGA', 'ACCGGGA', 'ACTGGGA', 'ACGGGGA',
+                         'ACAGGCA', 'ACCGGCA', 'ACTGGCA', 'ACGGGCA',
+                         'ACAGGAA', 'ACCGGAA', 'ACTGGAA', 'ACGGGAA']
+        self.assertSeqsEqual(expected_seqs, ambig_seq.AllConcreteSeqs())
+
+
+if __name__ == '__main__':
+    unittest.main()
