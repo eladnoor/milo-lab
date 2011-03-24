3890df397a1e32a4b80425170d122940453df81a
flamholz <flamholz@48c8c3c8-1ab2-6309-a68e-2c090a688e41>
2011-03-24 10:49:55 +0000

    Fix which start codon we check RBS strengths for.

--- src/pro_rbs/ambiguous_rbs.py
+++ src/pro_rbs/ambiguous_rbs.py
@@ -7,6 +7,7 @@ import sys
 
 from optparse import OptionParser
 from pro_rbs import rbs_util
+from scipy import stats
 from toolbox import ambiguous_seq
 
 
@@ -17,14 +18,17 @@ os.environ['NUPACKHOME'] = '/home/flamholz/Downloads/nupack3.0/'
 def MakeOpts():
     """Returns an OptionParser object with all the default options."""
     opt_parser = OptionParser()
-    opt_parser.add_option("-s", "--rbs_seq", dest="rbs_seq",
+    opt_parser.add_option("-r", "--rbs_seq", dest="rbs_seq",
                           default="../res/gibbs.sqlite",
                           help=("The RBS sequence including spacers, start codon, and"
                                 "(optionally) his_tag. May be ambiguous."))
+    opt_parser.add_option("-s", "--start_codon_seq", dest="start_codon_seq",
+                          default="ATGCATCATCACCATCACCAC",
+                          help="The sequence of the start codon, including some trailing bases.")
     return opt_parser
 
 
-def HandleAmbigSeq(ambig_seq):
+def HandleAmbigSeq(ambig_seq, start_codon_seq):
     """Process an ambiguous RBS sequence.
     
     Test all concrete possibilities for the sequence, draw a histogram of 
@@ -32,12 +36,21 @@ def HandleAmbigSeq(ambig_seq):
     
     Args:
         ambig_seq: a AmbigousDNASeq object.
+        start_codon_seq: the sequence of the start codon including
+            some trailing bases.
     """
     dg_by_seq = {}
     failure_count = 0
     for concrete_seq in ambig_seq.AllConcreteSeqs():
         try:
-            dG = rbs_util.TryCalcDG(concrete_seq)
+            seq_str = concrete_seq.tostring()
+            start_index = seq_str.find(start_codon_seq)
+            if start_index == -1:
+                logging.error('Couldn\'t find start codon.')
+                continue
+            
+            dG = rbs_util.TryCalcDG(concrete_seq,
+                                    start_codon_index=start_index)
             dg_by_seq[str(concrete_seq)] = dG
         except Exception, e:
             logging.error(e)
@@ -49,10 +62,16 @@ def HandleAmbigSeq(ambig_seq):
     
     binding_dGs = dg_by_seq.values()
     log_rates = map(rbs_util.LogRate, binding_dGs)
-    pylab.hist(log_rates)
-    pylab.xlim((-4, 4))
+    kde = stats.gaussian_kde(log_rates)
+    sample_points = pylab.linspace(-4, 4, 1000)
+    kde_pts = kde.evaluate(sample_points)
+
+    pylab.xlim((-4, 4))    
     pylab.xlabel('log10(K_binding)')
     pylab.ylabel('Number of RBS')
+    pylab.hist(log_rates, label='RBS histogram')
+    pylab.plot(sample_points, kde_pts, color='r', label='RBS KDE')
+    pylab.legend()
     pylab.show()
     
     seq_by_dg = {}
@@ -71,6 +90,8 @@ def HandleAmbigSeq(ambig_seq):
 def main():
     options, _ = MakeOpts().parse_args(sys.argv)
     print 'RBS sequence:', options.rbs_seq
+    start_codon_seq = options.start_codon_seq.upper()
+    print 'Start codon sequence:', start_codon_seq
     
     ambig_seq = ambiguous_seq.AmbigousDNASeq(options.rbs_seq)
     if ambig_seq.IsConcrete():
@@ -78,7 +99,7 @@ def main():
         return
     
     print 'Testing all options for ambiguous sequence'
-    HandleAmbigSeq(ambig_seq)
+    HandleAmbigSeq(ambig_seq, start_codon_seq)
         
 
 if __name__ == '__main__':
--- src/pro_rbs/multiple_rbs.py
+++ src/pro_rbs/multiple_rbs.py
@@ -8,6 +8,7 @@ import sys
 
 from optparse import OptionParser
 from pro_rbs import rbs_util
+from scipy import stats
 from toolbox import ambiguous_seq
 
 
@@ -22,13 +23,18 @@ def MakeOpts():
                           default="pro_rbs/rbs_v1.csv",
                           help=("The name of the file containing all the RBS sequences"
                                 " including spacers, start codon, and (optionally) his_tag."))
+    opt_parser.add_option("-s", "--start_codon_seq", dest="start_codon_seq",
+                          default="ATGCATCATCACCATCACCAC",
+                          help="The sequence of the start codon, including some trailing bases.")
     return opt_parser
 
 
 def main():
     options, _ = MakeOpts().parse_args(sys.argv)
     print 'RBS filename:', options.rbs_filename
-    
+    start_codon_seq = options.start_codon_seq.upper()
+    print 'Start codon sequence:', start_codon_seq
+
     f = open(options.rbs_filename)
     dg_by_seq = {}
     failure_count = 0
@@ -36,8 +42,14 @@ def main():
     for row in csv.reader(f):
         rbs = row[0].upper()
         print 'Testing RBS', rbs
+        
+        start_index = rbs.find(start_codon_seq)
+        if start_index == -1:
+            print 'Couldn\'t find start codon.'
+            continue
+        
         try:
-            dG = rbs_util.TryCalcDG(rbs)
+            dG = rbs_util.TryCalcDG(rbs, start_codon_index=start_index)
             print 'Predicted dG %.2g' % rbs_util.TokJ(dG)
             print 'Predicted K_binding %f' % rbs_util.PredictedRate(dG)
             dg_by_seq[rbs] = dG
@@ -51,10 +63,16 @@ def main():
     
     binding_dGs = dg_by_seq.values()
     log_rates = map(rbs_util.LogRate, binding_dGs)
-    pylab.hist(log_rates)
+    kde = stats.gaussian_kde(log_rates)
+    sample_points = pylab.linspace(-4, 4, 1000)
+    kde_pts = kde.evaluate(sample_points)
+
     pylab.xlim((-4, 4))
     pylab.xlabel('log10(K_binding)')
-    pylab.ylabel('Number of RBS')
+    pylab.ylabel('Number of RBS')    
+    pylab.hist(log_rates, label='RBS histogram')
+    pylab.plot(sample_points, kde_pts, color='r', label='RBS KDE')
+    pylab.legend()
     pylab.show()
     
 
