6d373680d6a330b4126e2f0095d541821822de03
elad.noor@gmail.com <elad.noor@gmail.com@48c8c3c8-1ab2-6309-a68e-2c090a688e41>
2011-04-03 21:18:51 +0000

    Improving the Hatzimanikatis class. Testing it properly on the carbon fixation cycles using thermodynamic analysis

--- trunk/src/pygibbs/thermodynamics.py
+++ trunk/src/pygibbs/thermodynamics.py
@@ -6,6 +6,7 @@ import json
 
 from thermodynamic_constants import default_T, default_pH, default_I, default_pMg
 from pygibbs import thermodynamic_constants
+from pygibbs.pseudoisomer import PseudoisomerMap
 
 class MissingCompoundFormationEnergy(Exception):
     def __init__(self, value, cid=0):
@@ -27,6 +28,14 @@ class Thermodynamics(object):
         self.cid2source_string = {}
         self.anchors = set()
 
+    def SetPseudoisomerMap(self, cid, pmap):
+        """ Add a whole Pseudoisomer Map for a compound according to its CID """
+        raise NotImplementedError
+
+    def AddPseudoisomer(self, cid, nH, z, nMg, dG0):
+        """ Add a single pseudoisomer to according to its CID """
+        raise NotImplementedError
+        
     def cid2PseudoisomerMap(self, cid):
         """
             Given a CID, returns the entire set of pseudoisomers (using class
@@ -35,6 +44,7 @@ class Thermodynamics(object):
         raise NotImplementedError
 
     def get_all_cids(self):
+        """ Returns a list of all CIDs that have data. """
         raise NotImplementedError
 
     def cid2dG0(self, cid, nH, nMg=0):
@@ -47,7 +57,7 @@ class Thermodynamics(object):
             but the default is 0
         """
         pmap = self.cid2PseudoisomerMap(cid)
-        for p_nH, p_z, p_nMg, dG0 in pmap.ToMatrix():
+        for p_nH, unused_p_z, p_nMg, dG0 in pmap.ToMatrix():
             if nH == p_nH and nMg == p_nMg:
                 return dG0
         raise MissingCompoundFormationEnergy("C%05d doesn't have a formation "
@@ -153,12 +163,15 @@ class Thermodynamics(object):
         json_file = open(json_fname, 'w')
         json_file.write(json.dumps(formations, indent=4))
         json_file.close()
-        
+
     def override_data(self, other):
+        for cid, source in other.cid2source_string.iteritems():
+            self.cid2source_string[cid] = source
+        
         for cid in other.get_all_cids():
-            self.cid2source_string[cid] = other.cid2source_string.get(cid, None)
-            self.cid2pmap_dict[cid] = other.cid2pmap_dict[cid]
-            
+            pmap = other.cid2PseudoisomerMap(cid)
+            self.SetPseudoisomerMap(cid, pmap)
+
     def write_transformed_data_to_csv(self, csv_fname):
         writer = csv.writer(open(csv_fname, 'w'))
         writer.writerow(['cid', 'pH', 'pMg', 'I', 'T', 'dG0_tag'])
@@ -194,7 +207,7 @@ class Thermodynamics(object):
         for c, cid in enumerate(cids):
             name = kegg.cid2name(cid)
             try:
-                for (nH, z, nMg, dG0) in self.cid2PseudoisomerMap(cid).ToMatrix():
+                for nH, z, nMg, dG0 in self.cid2PseudoisomerMap(cid).ToMatrix():
                     html_writer.write('<tr><td><a href="%s">C%05d</a></td><td>%s</td><td>%.2f</td><td>%d</td><td>%d</td><td>%d</td></tr>\n' % \
                                       (kegg.cid2link(cid), cid, name, dG0, nH, z, nMg))
                 dG0_f[c] = self.cid2dG0_tag(cid)
@@ -222,11 +235,16 @@ class PsuedoisomerTableThermodynamics(Thermodynamics):
         for row in db.DictReader(table_name):
             if row.get('use for', None) == 'skip':
                 continue
-            if not row['cid']:
+            
+            cid = row['cid']
+            if not cid:
                 continue
-            thermo.cid2pmap_dict.setdefault(row['cid'], pseudoisomer.PseudoisomerMap())
-            thermo.cid2pmap_dict[row['cid']].Add(row['nH'], row['z'], 
-                                                 row['nMg'], row['dG0'])
+            
+            nH = row['nH']
+            z = row['z']
+            nMg = row['nMg']
+            dG0 = row['dG0']
+            thermo.AddPseudoisomer(cid, nH, z, nMg, dG0)
             ref = row.get('ref', '')
             if row['cid'] in thermo.cid2source_string and \
                     thermo.cid2source_string[row['cid']] != ref:
@@ -256,9 +274,7 @@ class PsuedoisomerTableThermodynamics(Thermodynamics):
             z = int(row['z'])
             nMg = int(row['nMg'])
             dG0 = float(row['dG0'])
-            thermo.cid2pmap_dict.setdefault(cid, pseudoisomer.PseudoisomerMap())
-            thermo.cid2pmap_dict[cid].Add(nH, z, nMg, dG0)
-            
+            thermo.AddPseudoisomer(cid, nH, z, nMg, dG0)
             ref = row.get('ref', '')
             if cid in thermo.cid2source_string and thermo.cid2source_string[cid] != ref:
                 logging.warning('There are conflicting references for C%05d in '
@@ -269,12 +285,19 @@ class PsuedoisomerTableThermodynamics(Thermodynamics):
         return thermo
 
     def cid2PseudoisomerMap(self, cid):
-        if (cid in self.cid2pmap_dict):
+        if cid in self.cid2pmap_dict:
             return self.cid2pmap_dict[cid]
         else:
             raise MissingCompoundFormationEnergy("The compound C%05d does not "
                 "have a value for its formation energy of any of its "
                 "pseudoisomers" % cid, cid)
+
+    def SetPseudoisomerMap(self, cid, pmap):
+        self.cid2pmap_dict[cid] = pmap
+
+    def AddPseudoisomer(self, cid, nH, z, nMg, dG0):
+        self.cid2pmap_dict.setdefault(cid, PseudoisomerMap())
+        self.cid2pmap_dict[cid].Add(nH, z, nMg, dG0)
 
     def get_all_cids(self):
         return sorted(self.cid2pmap_dict.keys())
