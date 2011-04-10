7f59196a2ca5f13c6c8fa625a4d742b00b8b8fcd
elad.noor <elad.noor@48c8c3c8-1ab2-6309-a68e-2c090a688e41>
2011-04-10 15:05:28 +0000

    Now the export_kegg_data.py also exports the formation energies depracating the need for the export_formation_energies.py script. The gibbs_site code should be updated next to accomodate for this.

--- trunk/src/pygibbs/scripts/export_kegg_data.py
+++ trunk/src/pygibbs/scripts/export_kegg_data.py
@@ -4,14 +4,17 @@ import sys
 
 from optparse import OptionParser
 from pygibbs.kegg import Kegg
+from toolbox.database import SqliteDatabase
+from pygibbs.thermodynamics import PsuedoisomerTableThermodynamics
 
 
 def MakeOpts():
     """Returns an OptionParser object with all the default options."""
     opt_parser = OptionParser()
-    opt_parser.add_option("-d", "--database_location", dest="db_filename",
+    opt_parser.add_option("-k", "--kegg_database_location", 
+                          dest="kegg_db_filename",
                           default="../data/public_data.sqlite",
-                          help="The database location")
+                          help="The KEGG database location")
     opt_parser.add_option("-c", "--compounds_out_filename",
                           dest="compounds_out_filename",
                           default="../res/kegg_compounds.json",
@@ -24,7 +27,18 @@ def MakeOpts():
                           dest="enzymes_out_filename",
                           default="../res/kegg_enzymes.json",
                           help="Enzymes output filename.")
-    
+    opt_parser.add_option("-t", "--thermodynamics_filename",
+                          dest="thermo_filename",
+                          default='../data/thermodynamics/dG0.csv',
+                          help="Thermodynamics filename")
+    opt_parser.add_option("-d", "--database_location", 
+                          dest="db_filename",
+                          default="../res/gibbs.sqlite",
+                          help="The Thermodynamic database location")
+    opt_parser.add_option("-g", "--gc_table_name",
+                          dest="gc_table_name",
+                          default='gc_pseudoisomers',
+                          help="Group Contribution Table Name")    
     return opt_parser
 
 
@@ -37,10 +51,25 @@ def WriteJSONFile(objects, filename):
 
 def ExportJSONFiles():
     options, _ = MakeOpts().parse_args(sys.argv)
-    print 'Database filename:', options.db_filename
+    print 'KEGG Database filename:', options.kegg_db_filename
+    print 'Observed Thermodynamics filename:', options.thermo_filename
+    print 'Thermodynamic Database filename:', options.db_filename
+    print 'Group Contribution Table Name:', options.gc_table_name
+
+    db = SqliteDatabase(options.db_filename)
+    observed_thermo = PsuedoisomerTableThermodynamics.FromCsvFile(
+        options.thermo_filename)
+    if not db.DoesTableExist(options.gc_table_name):
+        raise ValueError('The table %s does not exist in the database. '
+                         'Please run the groups.py script and try again.'
+                         % options.gc_table_name)
+    thermo = PsuedoisomerTableThermodynamics.FromDatabase(
+        db, options.gc_table_name)
+    thermo.override_data(observed_thermo)
 
     # Make sure we have all the data.
     kegg = Kegg.getInstance()
+    kegg.AddThermodynamicData(thermo)
     
     print 'Exporting KEGG compounds as JSON.'
     print 'Output filename:', options.compounds_out_filename    
