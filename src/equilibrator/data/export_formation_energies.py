e5c37a929515c279ceda4ec5da729c80ffe2eea6
flamholz <flamholz@48c8c3c8-1ab2-6309-a68e-2c090a688e41>
2011-06-02 09:00:13 +0000

    Generalize the script for exporting formation energy data. Exports JSON and CSV, uses MIRIAM identifiers as suggested by Wolf.

--- src/equilibrator/data/export_formation_energies.py
+++ src/equilibrator/data/export_formation_energies.py
@@ -2,6 +2,7 @@
 
 import csv
 import sys
+import json
 
 from util import django_utils
 from optparse import OptionParser
@@ -10,6 +11,14 @@ django_utils.SetupDjango()
 
 from gibbs import models
 from gibbs import constants
+
+# Column names
+KEGG_ID = '!MiriamID::urn:miriam:kegg.compound'
+NAME = '!Name'
+INCHI = '!InChI'
+SOURCE = '!Source'
+FORMATION = '!FormationEnergy'
+ROW_ORDER = [NAME, KEGG_ID, INCHI, FORMATION, SOURCE]
 
 
 def GenFormationEnergyData(pH=constants.DEFAULT_PH,
@@ -22,14 +31,16 @@ def GenFormationEnergyData(pH=constants.DEFAULT_PH,
         pH: the pH.
         ionic_strength: the ionic strength.
     """
-    dicts = {}
+    dicts = []
     for compound in models.Compound.objects.all():
         dG = compound.DeltaG(pH=pH, ionic_strength=ionic_strength)
-        d = {'KEGG ID': compound.kegg_id,
-             'Name': compound.FirstName(),
-             'dG (kJ/mol)': dG,
-             'InChI': compound.inchi,
-             'Source': compound.dg_source}
+        if dG:
+            dG = round(dG, 3)
+        d = {KEGG_ID: compound.kegg_id,
+             NAME: compound.FirstName(),
+             FORMATION: dG,
+             INCHI: compound.inchi,
+             SOURCE: compound.dg_source.name}
         dicts.append(d)
     return dicts
 
@@ -42,9 +53,12 @@ def MakeOpts():
     opt_parser.add_option("-i", "--ionic_strength", dest="ionic_strength",
                           type="float", default=constants.DEFAULT_IONIC_STRENGTH,
                           help="The ionic strength, M")
-    opt_parser.add_option("-o", "--output_filename", dest="output_filename",
+    opt_parser.add_option("-c", "--output_csv", dest="output_csv",
                           type="string", default="formation_energies.csv",
-                          help="The name of the file to write output to.")
+                          help="The name of the file to write csv output to.")
+    opt_parser.add_option("-j", "--output_json", dest="output_json",
+                          type="string", default="formation_energies.json",
+                          help="The name of the file to write json output to.")
     return opt_parser
 
 
@@ -52,16 +66,21 @@ def main():
     options, _ = MakeOpts().parse_args(sys.argv)
     print 'Using pH = %.2f and ionic strength = %.3f' % (options.pH,
                                                          options.ionic_strength)
-    print 'Will write output to %s' % options.output_filename
+    print 'Will write csv output to %s' % options.output_csv
+    print 'Will write json output to %s' % options.output_json
 
     dicts = GenFormationEnergyData(pH=options.pH,
                                    ionic_strength=options.ionic_strength)
-    csv_data = sorted(dicts, key=lambda x: (x['KEGG ID'], x['Source']))
-    csv_file = open(options.output_filename, 'w')
-    writer = csv.DictWriter(csv_file, ['KEGG ID', 'Name', 'dG (kJ/mol)', 'InChI', 'Source'])
-    writer.writerows(csv_data)
-        
+    sorted_data = sorted(dicts, key=lambda x: (x[KEGG_ID], x[SOURCE]))
+    csv_file = open(options.output_csv, 'w')
+    writer = csv.DictWriter(csv_file, ROW_ORDER)
+    writer.writeheader()
+    writer.writerows(sorted_data)
     csv_file.close()
+    
+    json_file = open(options.output_json, 'w')
+    json.dump(sorted_data, json_file, sort_keys=True, indent=4)
+    json_file.close()
     
     print 'Done.'
             
