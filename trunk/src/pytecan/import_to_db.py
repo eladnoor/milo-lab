3f58a4ed5b2700c4d8dfc59b73dd248781f87040
elad.noor <elad.noor@48c8c3c8-1ab2-6309-a68e-2c090a688e41>
2011-07-13 14:40:16 +0000

    Using a dialog box to import gzip files into the database.

--- trunk/src/pytecan/import_to_db.py
+++ trunk/src/pytecan/import_to_db.py
@@ -1,7 +1,8 @@
 from optparse import OptionParser
-from pytecan.util import WriteToSqlite, CollectData
-import sys
+from pytecan.util import WriteToDatabase, CollectData
+import sys, os
 from toolbox.database import MySQLDatabase
+import tkFileDialog
 
 def MakeOpts():
     """Returns an OptionParser object with all the default options."""
@@ -24,9 +25,19 @@ def main():
     opt_parser = MakeOpts()
     options, _ = opt_parser.parse_args(sys.argv)
     if not options.tar_filename:
-        opt_parser.print_help(sys.stderr)
+        tar_fname = tkFileDialog.askopenfilename(filetypes=
+            [('gzip files', '.gz'), ('tar files', '.tar'), ('all files', '.*')])
+        if not tar_fname:
+            sys.exit(0)
+    else:
+        tar_fname = options.tar_filename
+    
+    if not os.path.exists(tar_fname):
+        print "File not found: " + tar_fname
         sys.exit(-1)
+    
     print "Importing from file: " + options.tar_filename
+    
     #print "Importing into database: " + options.sqlite_db_filename
     if options.exp_id:
         print "Experiment ID: " + options.exp_id
@@ -34,13 +45,13 @@ def main():
     MES = CollectData(options.tar_filename)
     db = MySQLDatabase(host='132.77.80.238', user='ronm', 
                        passwd='a1a1a1', db='tecan')
-    exp_id = WriteToSqlite(MES, db, options.exp_id)
+    exp_id = WriteToDatabase(MES, db, options.exp_id)
     db.Commit()
     print "Done importing experiment: %s" % exp_id
     
-    for row in db.Execute('SELECT exp_id, plate, count(*) from tecan_readings '
-                          'GROUP BY exp_id, plate'):
-        print row
+    #for row in db.Execute('SELECT exp_id, plate, count(*) from tecan_readings '
+    #                      'GROUP BY exp_id, plate'):
+    #    print row
     
 if __name__ == '__main__':
     main()
