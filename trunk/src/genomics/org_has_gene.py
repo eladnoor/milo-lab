365d288e40e51c65f1d1295d0867ec2eb2d7df8f
flamholz <flamholz@48c8c3c8-1ab2-6309-a68e-2c090a688e41>
2011-08-16 14:34:52 +0000

    More scriptyness for making custom graphs.

--- trunk/src/genomics/org_has_gene.py
+++ trunk/src/genomics/org_has_gene.py
@@ -15,27 +15,28 @@ def MakeOpts():
     opt_parser.add_option("-g", "--gene_filename",
                           dest="gene_filename",
                           help="input filename with species having gene")
+    opt_parser.add_option("-k", "--kegg_id_col",
+                          dest="kegg_id_col",
+                          default="KEGG Organism ID",
+                          help="input filename with species having gene")
     return opt_parser
 
-
 def Main():
-	options, _ = MakeOpts().parse_args(sys.argv)
-	assert options.species_filename and options.gene_filename
-	print 'Reading species list from', options.species_filename
-	print 'Reading gene data from', options.gene_filename
+    options, _ = MakeOpts().parse_args(sys.argv)
+    assert options.species_filename and options.gene_filename
+    print 'Reading species list from', options.species_filename
+    print 'Reading gene data from', options.gene_filename
 
-	gene_holders = set()
-	r = csv.DictReader(open(options.gene_filename))
-	map(lambda x: gene_holders.add(x.get('Species').lower()), r)
-	#print gene_holders
+    gene_holders = set()
+    r = csv.DictReader(open(options.gene_filename))
+    map(lambda x: gene_holders.add(x.get('Species').lower()), r)
 
-	r = csv.DictReader(open(options.species_filename))
-	for row in r:
-		kegg_id = row.get('KEGG ID').lower()
-		name = row.get('Organism Name')
-		#print name, kegg_id,
-		print kegg_id in gene_holders
-	 
+    id_col = options.kegg_id_col
+    r = csv.DictReader(open(options.species_filename))
+    for row in r:
+        kegg_id = row.get(id_col).lower()
+        print kegg_id in gene_holders
+     
 
 
 if __name__ == '__main__':
