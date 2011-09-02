79fe99ad92b6f6ad1308b25a9d38be04ef5eb02b
flamholz <flamholz@48c8c3c8-1ab2-6309-a68e-2c090a688e41>
2011-09-02 09:18:38 +0000

    sigmoid fitting to real data

--- src/scripts/fit_sigmoid.py
+++ src/scripts/fit_sigmoid.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pylab
 import scipy.optimize
+import sys
 
 
 def sigmoid(p,x):
@@ -22,11 +23,11 @@ def residuals(p,x,y):
 
 
 def fit_sigmoid(x, y):
-    p_guess=(np.median(low_rate), np.median(noisy_vals),
-             max(noisy_vals), min(noisy_vals))
+    p_guess=(np.median(x), np.median(y),
+             max(y), min(y))
     p, cov, infodict, mesg, ier = scipy.optimize.leastsq(
-            residuals, p_guess, args=(low_rate,noisy_vals),
-            full_output=1, warning=True)
+            residuals, p_guess, args=(x,y),
+            full_output=1)
     return p
 
 
@@ -53,6 +54,13 @@ def yoffset(params):
     return y0
 
 
+noisy_vals = pylab.array(
+[4.62e-2,
+5.54e-2,
+5.7e-2, 5.49e-2, 5.93e-2, 6.21e-2, 6.82e-2, 7.75e-2, 9.13e-2, 0.1091, 0.127, 0.1481, 0.17, 0.1989, 0.2339, 0.2719, 0.3174, 0.3489, 0.383, 0.417, 0.4488, 0.4889, 0.5332, 0.5679, 0.6023, 0.6305, 0.6551, 0.6795, 0.7075, 0.7318, 0.7591, 0.7798, 0.8016, 0.8254, 0.8434, 0.8646, 0.8816, 0.9015, 0.9159, 0.9296, 0.9356, 0.9373, 0.9364, 0.9375, 0.9368, 0.9368, 0.9385, 0.9395, 0.941, 0.9376, 0.9355, 0.9361, 0.9345, 0.937, 0.9369, 0.9329, 0.9331, 0.9335, 0.9336, 0.9331, 0.9308, 0.9318, 0.93, 0.9296, 0.9315, 0.9283, 0.9285, 0.9273, 0.9275, 0.9274, 0.9275, 0.9248])
+
+noisy_vals = pylab.log(noisy_vals)
+"""
 noisy_vals = pylab.array([0.0314,
 0.032,
 0.0397,
@@ -80,12 +88,13 @@ noisy_vals = pylab.array([0.0314,
 0.2493,
 0.2942,
 0.3393])
+"""
 times = pylab.arange(len(noisy_vals))
 
 
 #p = (0.0, 1.0, 10, 1.0)
-#high_rate = pylab.arange(-10, 10, 0.25)
-#low_rate = pylab.arange(-10, 10, 0.5)
+high_rate = pylab.arange(min(times), 1.5*max(times), 0.25)
+low_rate = pylab.arange(min(times), 1.5*max(times), 0.5)
 #vals = sigmoid(p, high_rate)
 #noisy_vals = noisy_sigmoid(p, low_rate)
 
@@ -98,19 +107,31 @@ stationary = pylab.zeros(len(times)) + stationary_level(p)
 pylab.plot(times, stationary, 'k--', label="Fit stationary")
 
 yoff = pylab.zeros(len(times)) + yoffset(p)
+print yoff
 pylab.plot(times, yoff, 'm--', label="Fit Y offsett")
 
 xmid, ymid = midpoint(p)
 slope, offset = growth_rate(p)
+print slope, offset
 pylab.plot([xmid], [ymid], 'mo', label='Inflection point')
-pylab.plot(high_rate, linear(slope, high_rate, offset), 'k-', label='Growth phase')
+
+
+l = linear(slope, high_rate, offset)
+growth_x = []
+growth_y = []
+for x, y in zip(high_rate, l):
+    if y < yoff[0] or y > stationary[0]:
+        continue
+    growth_x.append(x)
+    growth_y.append(y)
+        
+pylab.plot(growth_x, growth_y, 'k-', label='Growth phase')
 pxp=sigmoid(p, high_rate)
 pylab.plot(high_rate, pxp, 'g', label='Fit')
-pylab.legend(loc="upper left")
-
+pylab.legend(loc="lower right")
 
 pylab.subplot(212)
-residuals = pylab.absolute(noisy_vals - sigmoid(p, low_rate))
+residuals = pylab.absolute(noisy_vals - sigmoid(p, times))
 average_resid = pylab.mean(residuals)
 pylab.stem(range(len(residuals)), residuals)
 pylab.show()
