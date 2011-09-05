da84b1be4fe7270dcecb9b90b170c6a1c2f517b4
flamholz <flamholz@48c8c3c8-1ab2-6309-a68e-2c090a688e41>
2011-09-05 13:59:53 +0000

    update scripty.

--- src/genomics/merge_kegg_ncbi.py
+++ src/genomics/merge_kegg_ncbi.py
@@ -22,51 +22,57 @@ def MakeOpts():
 
 
 def Main():
-	options, _ = MakeOpts().parse_args(sys.argv)
-	assert options.kegg_filename
-	assert options.ncbi_filename
-	assert options.output_filename
-	print 'Reading KEGG names from', options.kegg_filename
-	print 'Reading NCBI data from', options.ncbi_filename
+    options, _ = MakeOpts().parse_args(sys.argv)
+    assert options.kegg_filename
+    assert options.ncbi_filename
+    assert options.output_filename
+    print 'Reading KEGG names from', options.kegg_filename
+    print 'Reading NCBI data from', options.ncbi_filename
 
-	rows_by_taxa = {}
-	r = csv.DictReader(open(options.ncbi_filename), dialect=csv.excel_tab)
-	ncbi_fieldnames = list(r.fieldnames)
-	for row in r:
-		taxa = row.get('Taxonomy ID', None)
-		if not taxa:
-			logging.warning('Undefined taxa for row %s', row)
-			continue
-		if taxa in rows_by_taxa:
-			logging.warning('Duplicate taxa %s', taxa)
-			continue
+    rows_by_taxa = {}
+    r = csv.DictReader(open(options.ncbi_filename), dialect=csv.excel_tab)
+    ncbi_fieldnames = list(r.fieldnames)
+    for row in r:
+        taxa = row.get('NCBI Taxon ID', None)
+        if not taxa:
+            logging.warning('Undefined taxa for row %s', row)
+            continue
+        if taxa in rows_by_taxa:
+            logging.warning('Duplicate taxa %s', taxa)
+            continue
 
-		num_taxa = int(taxa.strip())
-		rows_by_taxa[num_taxa] = row
+        try:
+            stripped = taxa.strip()
+            num_taxa = int(taxa.strip())
+            rows_by_taxa[stripped] = row
+        except Exception, e:
+            print e
+            print row
+            continue
+        
+    kegg_by_taxa = {}
+    for row in csv.DictReader(open(options.kegg_filename)):
+        taxa = row.get('NCBI Taxon ID', None)
+        kegg_id = row.get('KEGG Organism ID')
+        if taxa is not None:
+            if taxa not in kegg_by_taxa:
+                stripped = taxa.strip()
+                kegg_by_taxa[stripped] = kegg_id
+            else:
+                logging.warning('Duplicate taxa %s', taxa)
+                
 
-	print 'Writing output to', options.output_filename
-	ncbi_fieldnames.append('KEGG ID')
-	w = csv.DictWriter(open(options.output_filename, 'w'), ncbi_fieldnames)
-	w.writeheader()
+    print 'Writing output to', options.output_filename
+    ncbi_fieldnames.append('KEGG ID')
+    w = csv.DictWriter(open(options.output_filename, 'w'), ncbi_fieldnames)
+    w.writeheader()
 
-	for row in csv.DictReader(open(options.kegg_filename)):
-		taxa = row.get('TAX ID', None)
-		kegg_id = row.get('KEGG ID')
-		if taxa is None:
-			logging.warning('No taxa for KEGG ID %s', kegg_id)
-			continue
-		
-		try:
-			n_taxa = int(taxa.strip())
-		except ValueError:
-			logging.warning('Invalid taxa %s', taxa)
-			continue 
-
-		if n_taxa in rows_by_taxa:
-			row = rows_by_taxa[n_taxa]
-			row['KEGG ID'] = kegg_id
-			w.writerow(row)
-	
+    for taxa, row in rows_by_taxa.iteritems():
+        kegg_id = kegg_by_taxa.get(taxa, None)
+        
+        row['KEGG ID'] = kegg_id
+        w.writerow(row)
+    
 
 
 if __name__ == '__main__':
