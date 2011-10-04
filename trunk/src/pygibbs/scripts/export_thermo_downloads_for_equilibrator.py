9c79e9fd433413f3162b6d6e9f89a14ca8517c83
flamholz <flamholz@48c8c3c8-1ab2-6309-a68e-2c090a688e41>
2011-10-04 14:44:41 +0000

    add a script to export download files for equilibrator.

---
+++ trunk/src/pygibbs/scripts/export_thermo_downloads_for_equilibrator.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import subprocess
+
+thermo_sources = ('alberty', 'PGC')
+phs = ['5.0', '5.5', '6.0', '6.5', '7.0', '7.5',
+       '8.0', '8.5', '9.0']
+
+for thermo in thermo_sources:
+    for ph in phs:
+        compounds_fname = 'kegg_compounds_%s_ph%s.csv' % (thermo,
+                                                          ph)
+        reactions_fname = 'kegg_reactions_%s_ph%s.csv' % (thermo,
+                                                          ph)
+        
+        args = ['python',
+                './pygibbs/scripts/export_kegg_data_biochemical.py',
+                '-c', compounds_fname,
+                '-r', reactions_fname,
+                '-t', thermo,
+                '-H', ph]
+        subprocess.call(args)
