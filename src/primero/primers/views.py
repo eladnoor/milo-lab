9933e0ba754c47f66091dd74f1e295a673de6645
elad.noor@gmail.com <elad.noor@gmail.com@48c8c3c8-1ab2-6309-a68e-2c090a688e41>
2011-10-11 09:07:33 +0000

    bug fixes to primero

--- src/primero/primers/views.py
+++ src/primero/primers/views.py
@@ -4,12 +4,16 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponse, HttpResponseRedirect
 from django.http import Http404
 from models import Order, Primer, Pending
-import time, sys, re
+import time, re
 from forms import SearchForm, FetchMailForm
-import email, imaplib, os
+import email, imaplib
+
 from pdfminer.pdfinterp import PDFResourceManager, process_pdf
 from pdfminer.converter import TextConverter
 from pdfminer.layout import LAParams
+from pdfminer.pdfparser import PDFSyntaxError
+
+import io
 import StringIO
 from forms import UploadSigmaPdfForm
 from primero.primers.forms import UpdateLocationForm
@@ -24,8 +28,10 @@ def GetNowString():
     return time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime())
     
 def primer_search(request):
-    primers_list = Pending.objects.filter()
-    return render_to_response('search.html', {'n_pending': len(primers_list)})
+    d = {}
+    d['n_pending'] = len(Pending.objects.filter())
+    d['n_primers'] = len(Primer.objects.filter())
+    return render_to_response('search.html', d)
 
 def fetch_login(request):
     return render_to_response('fetchlogin.html')
@@ -41,7 +47,7 @@ def results(request):
     else:
         query_string = search_form.cleaned_data["query"]
         try:
-            primers_list = Primer.objects.filter(name__contains=query_string)
+            primers_list = Primer.objects.filter(name__icontains=query_string)
         except Primer.DoesNotExist:
             primers_list = []
     
@@ -79,7 +85,7 @@ def update_location(request):
         primer.col = update_form.cleaned_data["col"]
         if primer.col < 1 or primer.col > 9:
             return error("Column out of range: %d" % primer.col)
-        primer.loc_id = Primer.get_loc_id(primer.box, primer.row, primer.col)
+        primer.loc_id = Primer.BoxRowCol2LocID(primer.box, primer.row, primer.col)
         primer.save()
         #return render_to_response('updated.html',
         #    {'primers_list': primers_list, 'query': query})
@@ -98,30 +104,23 @@ def update_pending(request):
         pending_list.append(p)
 
     if 'Delete' in request.POST:
-        for p in pending_list:
-            p.delete()
+        for pending in pending_list:
+            pending.delete()
         return HttpResponseRedirect('../pending/')
     elif 'Submit' in request.POST:
         updated_list = []
-        for p in pending_list:
-            existing_primers = Primer.objects.all()
-            set_of_ids = set()
-            for p in existing_primers:
-                set_of_ids.add(p.loc_id)
-            if len(set_of_ids) == 0:
-                loc_id = 0
-            else:
-                all_ints_in_range = set(range(1, max(set_of_ids) + 2))
-                available_ids = all_ints_in_range.difference(set_of_ids)
-                loc_id = min(available_ids)
+        for pending in pending_list:
+            name = pending.name
+            seq = pending.seq
+            pending.delete()
             
+            loc_id = Primer.GetNextAvailableLocID()
             # find plate, row, col
-            box, row, col = Primer.loc_id2location(loc_id)
-            entry = Primer(name=p.name, seq=p.seq,
+            box, row, col = Primer.LocID2BoxRowCol(loc_id)
+            entry = Primer(name=name, seq=seq,
                            comment='', box=box, row=row, col=col,
                            loc_id=loc_id)
             entry.save()
-            p.delete()
             updated_list.append(entry)
         return render_to_response('updated.html', {'primers_list': updated_list})
     
@@ -135,25 +134,27 @@ def fetch_mail(request):
     
     user = fetch_form.cleaned_data["email"]
     pwd = fetch_form.cleaned_data["password"]
-    detach_dir = '/tmp' # directory where to save attachments (default: current)
     
     # connecting to the gmail imap server
-    m = imaplib.IMAP4_SSL("imap.gmail.com")
-    resp, items = m.login(user, pwd)
+    imap = imaplib.IMAP4_SSL("imap.gmail.com")
+    resp, _items = imap.login(user, pwd)
     if resp != 'OK':
         return error("Wrong user name or password")
        
     #m.select("[Gmail]/INBOX") # here you a can choose a mail box like INBOX instead
-    resp, items = m.select('sigma')
+    resp, _items = imap.select('sigma')
     if resp != 'OK':
         return error("Cannot locate label 'sigma'")
     
-    resp, items = m.search(None, '(SUBJECT "Sigma-Aldrich Order confirmation")')
+    resp, items = imap.search(None, '(SUBJECT "Sigma-Aldrich Order confirmation")')
+    if resp != 'OK':
+        return error("Cannot search for emails for Sigma")
     items = items[0].split() # getting the mails id
     
+    pending_list = []
     for emailid in items:
         # fetching the mail, "`(RFC822)`" means "get the whole stuff", but you can ask for headers only, etc
-        resp, data = m.fetch(emailid, "(RFC822)")
+        resp, data = imap.fetch(emailid, "(RFC822)")
         if data is None or data[0] is None:
             continue
         email_body = data[0][1] # getting the mail content
@@ -175,42 +176,37 @@ def fetch_mail(request):
             if part.get('Content-Disposition') is None:
                 continue
     
-            filename = part.get_filename()
-            counter = 1
-    
-            # if there is no filename, we create one with a counter to avoid duplicates
-            if not filename:
-                filename = 'part-%03d%s' % (counter, 'bin')
-                counter += 1
-    
-            att_path = os.path.join(detach_dir, filename)
-    
-            # Check if its already there
-            if not os.path.isfile(att_path) :
-                # finally write the stuff
-                fp = open(att_path, 'wb')
-                fp.write(part.get_payload(decode=True))
-                fp.close()
-                apply_lbl_msg = m.uid('COPY', emailid, 'done')
-                if apply_lbl_msg[0] == 'OK':
-                    mov, data = m.uid('STORE', emailid , '+FLAGS', '(\Deleted)')
-                    m.expunge()                
-                for primer in _pdfMine(att_path):
+            # finally write the stuff
+            fp = io.BytesIO()
+            fp.write(part.get_payload(decode=True))
+            fp.seek(0)
+            #apply_lbl_msg = imap.uid('COPY', emailid, 'done')
+            #if apply_lbl_msg[0] == 'OK':
+            #    mov, data = imap.uid('STORE', emailid , '+FLAGS', '(\Deleted)')
+            #    imap.expunge()                
+            try:
+                for primer in pdfMine(fp):
                     entry = Pending(name=primer[0], seq=primer[1])
                     entry.save()
-    return HttpResponse("Done")
+                    pending_list.append(entry)
+            except PDFSyntaxError:
+                pass
+            fp.close()
+
+    return render_to_response('upload_success.html',
+                                      {'primers_list': pending_list})
 
 def upload_sigma_pdf(request):
     if request.method == 'POST':
         form = UploadSigmaPdfForm(request.POST, request.FILES)
         if form.is_valid():
-            primers_list = []
+            pending_list = []
             for primer in pdfMine(request.FILES['file']):
                 entry = Pending(name=primer[0], seq=primer[1])
                 entry.save()
-                primers_list.append(entry)
+                pending_list.append(entry)
             return render_to_response('upload_success.html',
-                                      {'primers_list': primers_list})
+                                      {'primers_list': pending_list})
         else:
             return error("Invalid operation")
     else:
@@ -219,10 +215,15 @@ def upload_sigma_pdf(request):
 
 def _pdfMine(fname):
     fp = file(fname, 'rb')
-    pdfMine(fp, sys.stdout)
+    primers = pdfMine(fp)
     fp.close()
+    return primers
 
 def pdfMine(fp):
+    """
+        Input: file handle to a PDF file
+        Output: a list of tuples with (Primer Name, Primer Sequence)
+    """
     str_io = StringIO.StringIO()
     laparams = LAParams()
     
