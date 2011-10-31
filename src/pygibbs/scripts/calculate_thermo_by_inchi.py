6d245a50d8dd118dc806e0318bd804d5513b1436
elad.noor@gmail.com <elad.noor@gmail.com@48c8c3c8-1ab2-6309-a68e-2c090a688e41>
2011-10-31 21:47:08 +0000

    Added option for transformed energies in 'calculate_thermo_by_inchi' script.

--- src/pygibbs/scripts/calculate_thermo_by_inchi.py
+++ src/pygibbs/scripts/calculate_thermo_by_inchi.py
@@ -23,11 +23,29 @@ def MakeOpts():
                           dest="csv_output_filename",
                           default=None,
                           help="output CSV file with dGs and groups")
+    opt_parser.add_option("-b", "--biochemical",
+                          dest="biochemical",
+                          default=False,
+                          action="store_true",
+                          help="calculate the biochemical (transformed) energy at standard conditions")
+    opt_parser.add_option("-p", "--ph", action="store", type="float",
+                          dest="pH", default=default_pH,
+                          help="The width of the range of pH values to plot")
+    opt_parser.add_option("-i", "--ionic_strength", action="store", type="float",
+                          dest="I", default=default_I,
+                          help="The Ionic strength of the solution (in M)")
+    opt_parser.add_option("-m", "--pmg", action="store", type="float",
+                          dest="pMg", default=default_pMg,
+                          help="The pMg of the solution")
+    opt_parser.add_option("-t", "--temperature", action="store", type="float",
+                          dest="T", default=default_T,
+                          help="The T of the solution (in K)")
     return opt_parser
 
 def CalculateThermo():
     parser = MakeOpts()
     options, _ = parser.parse_args(sys.argv)
+    pH, I, pMg, T = options.pH, options.I, options.pMg, options.T
 
     db = SqliteDatabase('../res/gibbs.sqlite')
     G = GroupContribution(db=db)
@@ -51,39 +69,42 @@ def CalculateThermo():
     else:
         parser.error("must use either -s or -c option")
     
-    print "Calculating energies for %s compounds" % len(list_of_mols)
+    if options.biochemical:
+        print ("Calculating biochemical formation energies for %s compounds" 
+               " at pH = %.1f, I = %.2f, pMg = %.1f, T = %.2f" %  
+               (len(list_of_mols), pH, I, pMg, T))
+    else:
+        print ("Calculating chemical formation energies for %s compounds" % 
+               len(list_of_mols))
     
     rowdicts = []
     for mol_dict in list_of_mols:
-        id = mol_dict['id']
+        mol_id = mol_dict['id']
         diss_table = Molecule._GetDissociationTable(mol_dict['mol'],
                                                     format=mol_dict['format'])
         try:
-            mol = diss_table.GetMostAbundantMol(pH=default_pH, I=default_I, 
-                                                pMg=default_pMg, T=default_T)
+            mol = diss_table.GetMostAbundantMol(pH, I, pMg, T)
             decomposition = G.Mol2Decomposition(mol, 
                 ignore_protonations=ignore_protonations)
             groupvec = decomposition.AsVector()
             dG0 = G.groupvec2val(groupvec)
             nH = decomposition.Hydrogens()
             nMg = decomposition.Magnesiums()
-            if nMg > 0:
-                raise Exception("Magnesium ions are not allowed here")
-
             diss_table.SetFormationEnergyByNumHydrogens(dG0, nH, nMg)
             pmap = diss_table.GetPseudoisomerMap()
             
-            for p_nH, p_z, p_nMg, p_dG0 in pmap.ToMatrix():
-                rowdicts.append({'ID':id, 'nH':p_nH, 'charge':p_z, 'nMg':p_nMg,
-                                 'dG0':p_dG0, 'groupvec':str(groupvec),
-                                 'error':None})
+            if options.biochemical:
+                dG0_prime = pmap.Transform(pH, pMg, I, T)
+                rowdicts.append({'ID':mol_id, 'pH':pH, 'I':I, 'pMg':pMg,
+                                 'dG0\'':"%.1f" % dG0_prime, 'groupvec':str(groupvec)})
+            else:
+                for p_nH, p_z, p_nMg, p_dG0 in pmap.ToMatrix():
+                    rowdicts.append({'ID':mol_id, 'nH':p_nH, 'charge':p_z, 'nMg':p_nMg,
+                                     'dG0':"%.1f" % p_dG0, 'groupvec':str(groupvec)})
         except GroupDecompositionError:
-            rowdicts.append({'ID':id, 'nH':None, 'charge':None, 'nMg':None,
-                             'dG0':None, 'groupvec':None,
-                             'error':"cannot decompose"})
+            rowdicts.append({'ID':mol_id, 'error':"cannot decompose"})
         except GroupMissingTrainDataError:
-            rowdicts.append({'ID':id, 'nH':None, 'charge':None, 'nMg':None,
-                             'dG0':None, 'groupvec':str(groupvec),
+            rowdicts.append({'ID':mol_id, 'groupvec':str(groupvec),
                              'error':"missing training data"})
         
     if options.csv_output_filename is not None:
@@ -91,9 +112,12 @@ def CalculateThermo():
         print "writing results to %s ... " % options.csv_output_filename
     else:
         out_fp = sys.stdout
-        
-    csv_writer = csv.DictWriter(out_fp, 
-        ['ID', 'error', 'nH', 'nMg', 'charge', 'dG0', 'groupvec'])
+    
+    if options.biochemical:
+        titles = ['ID', 'error', 'pH', 'I', 'pMg', 'dG0\'', 'groupvec']
+    else:
+        titles = ['ID', 'error', 'nH', 'nMg', 'charge', 'dG0', 'groupvec'] 
+    csv_writer = csv.DictWriter(out_fp, titles)
     csv_writer.writeheader()
     csv_writer.writerows(rowdicts)
     
