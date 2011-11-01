1f9a121dc5d6cb7534a3d7ad8c129e2b4699824c
elad.noor@gmail.com <elad.noor@gmail.com@48c8c3c8-1ab2-6309-a68e-2c090a688e41>
2011-11-01 10:01:04 +0000

    more statistical fun

--- trunk/src/toolbox/stats.py
+++ trunk/src/toolbox/stats.py
@@ -1,4 +1,22 @@
+import numpy as np
 import scipy.stats as st
+
+def MeanWithInterval(Y, confidence=0.95):
+    """
+        Use the fact that (mean(Y) - mu) / (std(Y)/sqrt(n))
+        is a Student T distribution with n-1 degrees of freedom
+    """
+    n = len(Y)
+    Y_bar = np.mean(Y)
+
+    # According to the Student T-test distribution for n-1 degrees of freedom
+    # find the position where the CDF is 0.975 (assuming we want a confidence
+    # of 0.95). The lower part of the tail will account for the other 0.025
+    # chance.
+    t = st.t.ppf((confidence + 1.0)/2.0, n-1)
+    SD = np.std(Y, ddof=1) # use the unbiased estimator: sqrt(y^2 / (n-1))
+    SE = SD / np.sqrt(len(Y))
+    return Y_bar - t*SE, Y_bar + t*SE
 
 def GetSlopeInterval(x, y, confidence=0.95):
     slope, _intercept, _r_value, _p_value, std_err = st.linregress(x,y)
@@ -10,6 +28,9 @@ def GetSlopeInterval(x, y, confidence=0.95):
 def test():
     x = [0,12.0,29.5,43.0,53.0,62.5,75.5,85.0,93.0]
     y = [8.98,8.14,6.67,6.08,5.90,5.83,4.68,4.20,3.72]
+    
+    print MeanWithInterval(x)
+    print MeanWithInterval(y)
     print GetSlopeInterval(x, y)
     
 if __name__ == "__main__":
