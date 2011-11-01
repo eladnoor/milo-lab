c4b641588e110b976cf93216da6147700750d0b4
flamholz <flamholz@48c8c3c8-1ab2-6309-a68e-2c090a688e41>
2011-11-01 11:19:26 +0000

    random colors, updates to stats, some plate reading code.

---
+++ trunk/src/toolbox/color.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+COLORS = 'grbcmyk'
+
+import colorsys
+import numpy
+
+def RgbToHex(rgb):
+	r, g, b = rgb
+	r *= 255
+	g *= 255
+	b *= 255
+	print r, g, b
+	return '#%02x%02x%02x' % (r,g,b)
+	
+
+def ColorMap(items, saturation=0.5, value=0.95):
+	n = len(items)
+	hues = numpy.arange(float(n)) / float(n)
+	f = lambda h: RgbToHex(colorsys.hsv_to_rgb(h, saturation, value)) 
+	rgbs = map(f, hues)
+	return dict(zip(items, rgbs))
--- trunk/src/toolbox/plate.py
+++ trunk/src/toolbox/plate.py
@@ -2,8 +2,13 @@
 
 from toolbox.database import MySQLDatabase
 from toolbox import growth
+from toolbox.stats import MeanWithConfidenceInterval
+from toolbox.color import ColorMap
 import numpy
 import pylab
+import sys
+
+from optparse import OptionParser
 
 
 class TimedMeasurement(object):
@@ -74,70 +79,88 @@ class Plate96(object):
         times = numpy.vstack(times)
         return times, readings, numpy.array(labels)
                 
-            
-if __name__ == '__main__':
+
+def MakeOpts():
+    """Returns an OptionParser object with all the default options."""
+    opt_parser = OptionParser()
+    opt_parser.add_option("-e", "--experiment_id",
+                          dest="experiment_id",
+                          help="experiment ID")
+    opt_parser.add_option("-p", "--plate_id",
+                          dest="plate_id",
+                          help="plate ID")
+    opt_parser.add_option("-r", "--reading_label",
+                          dest="reading_label",
+                          default="OD600",
+                          help="Reading label")
+    opt_parser.add_option("-w", "--window_size",
+                          dest="window_size",
+                          type='int',
+                          default=12,
+                          help="Window size for computing the growth rate.")
+    opt_parser.add_option("-l", "--lower_bound",
+                          dest="lower_bound",
+                          type='int',
+                          default=0.1,
+                          help="Minimum reading to consider valid.")
+    return opt_parser
+
+
+def Main():
+    options, _ = MakeOpts().parse_args(sys.argv)
+    assert options.experiment_id and options.plate_id and options.reading_label
+    print 'Reading plate %s from experiment %s' % (options.plate_id, options.experiment_id)
+    
     db = MySQLDatabase(host='132.77.80.238', user='ronm', 
                        passwd='a1a1a1', db='tecan')
-    exp_id = '2011-10-05 17:55:38'
-    plate = '2'
-    
-    p = Plate96.FromDatabase(db, exp_id, plate)
-    times, ods, labels = p.SelectReading('OD600')
-    gfp_times, gfps, _labels = p.SelectReading('GFP')
-    blanks = pylab.find(labels == 'BLANK')
-    promoterless = pylab.find(labels == 'AZ01 F3 U66')
-    
-    f1 = pylab.figure(0)
-    pylab.semilogy(times[blanks, :], ods[blanks, :], 'b.', figure=f1)
-    pylab.semilogy(times[promoterless, :], ods[promoterless, :], 'g.', figure=f1)
-    pylab.title('OD600')
-    
-    calculator = growth.SlidingWindowGrowthCalculator(window_size=6, minimum_level=0.01)
-    n = promoterless.size
-    all_rates = []
-    all_errors = []
-    f2 = pylab.figure(1)
+    p = Plate96.FromDatabase(db, options.experiment_id,
+                             options.plate_id)
+    times, readings, labels = p.SelectReading(options.reading_label)
+
+    print 'Calculating growth rates'
+    lux_calculator = growth.SlidingWindowGrowthCalculator(window_size=options.window_size,
+                                                          minimum_level=options.lower_bound)
+    n = labels.size
+    colors = ColorMap(set(labels))
+    rates = {}
+    stationaries = {}
     for i in xrange(n):
-        rates = calculator.CalculateRates(gfp_times[promoterless[i], :],
-                                          gfps[promoterless[i], :])
-        all_rates.append(rates[:, 0])
-        all_errors.append(rates[:, 2])
-        pylab.errorbar(gfp_times[promoterless[i], :],
-                       rates[:, 0].T,
-                       yerr=rates[:, 2].T,
-                       fmt='g.', figure=f2)
-    pylab.title('GFP Growth Rate')
+        rate, stationary = lux_calculator.CalculateGrowth(times[i,:], readings[i,:])
+        scaled_rate = rate * 60 * 60
+        label = labels[i]
+        rates.setdefault(label, []).append(scaled_rate)
+        stationaries.setdefault(label, []).append(stationary)
     
-    f3 = pylab.figure(2)
-    pylab.semilogy(gfp_times[blanks, :], gfps[blanks, :], 'b.', figure=f3)
-    pylab.semilogy(gfp_times[promoterless, :], gfps[promoterless, :], 'g.', figure=f3)
-    pylab.title('GFP')
+    print 'Calculating mean growth rate and error'
+    f = pylab.figure(0)
+    mean_rates = []
+    rate_err = []
+    for label in rates.iterkeys():
+        r = rates[label]
+        mean_rate, err = MeanWithConfidenceInterval(r)
+        mean_rates.append(mean_rate)
+        rate_err.append(err)
+        s = stationaries[label]
+        pylab.semilogx(s, r, color=colors[label], marker='.',
+                       linestyle='None', figure=f, markersize=20, label=label) 
+    pylab.xlabel('Specific Growth Rate (/hour)')
+    pylab.ylabel('Stationary Level (%s)' % options.reading_label)
+    pylab.legend()
     
-    
-    f4 = pylab.figure(3)
-    gfp_to_od = gfps / ods
-    pylab.semilogy(gfp_times[blanks, :], gfp_to_od[blanks, :], 'b.', figure=f3)
-    pylab.semilogy(gfp_times[promoterless, :], gfp_to_od[promoterless, :], 'g.', figure=f3)
-    pylab.title('GFP/OD600')
-    
-    
-    rows, _ = times.shape
-    rates = []
-    stationaries = []
-    for i in xrange(rows):
-        rate, stationary = calculator.CalculateGrowth(times[i, :], ods[i, :])
-        rates.append(rate)
-        stationaries.append(stationary)
-    
-    """
-    f3 = pylab.figure(2)
-    pylab.hist(rates, figure=f3)
-    
-    f4 = pylab.figure(3)
-    pylab.hist(stationaries, figure=f3)
-    """
+    pylab.figure(2)
+    concs = pylab.array([float(l) for l in rates.keys()]) * 100
+    idx = range(len(concs))
+    pylab.loglog(concs, mean_rates, 'g.')
+    pylab.errorbar(concs, mean_rates, yerr=rate_err, fmt=None)
+    pylab.xlabel('Substrate Concentration (%)')
+    pylab.ylabel('Specific Growth Rate (/hour)')
+    pylab.xlim((-0.1, 3.0))
+
     pylab.show()
     
+    
+if __name__ == '__main__':
+    Main()
     
     
     
