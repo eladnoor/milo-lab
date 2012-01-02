1a6fc875ef2dfb3c199714023aaaec790009e222
elad.noor@gmail.com <elad.noor@gmail.com@48c8c3c8-1ab2-6309-a68e-2c090a688e41>
2012-01-02 15:42:20 +0000

    added a new test for group decomposition in 'pygibbs tests'.

--- trunk/src/pygibbs/tests/group_decomposition_test.py
+++ trunk/src/pygibbs/tests/group_decomposition_test.py
@@ -1,12 +1,13 @@
 import unittest
-from pygibbs import groups_data
+from pygibbs.groups_data import GroupsData
 from pygibbs import group_decomposition
 from toolbox.molecule import Molecule
+import logging
 
 
-PHOSPHATE = Molecule.FromSmiles('OP(O)(=O)O')
-ATP = Molecule.FromSmiles('C1=NC2=C(C(=N1)N)N=CN2C3C(C(C(O3)COP(=O)(O)OP(=O)(O)OP(=O)(O)O)O)O')
-A4P = Molecule.FromSmiles('C1=NC2=C(C(=N1)N)N=CN2C3C(C(C(O3)COP(=O)(O)OP(=O)(O)OP(=O)(O)OP(=O)(O)O)O)O')
+PHOSPHATE = Molecule.FromSmiles('[O-]P([O-])(=O)O')
+ATP = Molecule.FromSmiles('C1=NC2=C(C(=N1)N)N=CN2C3C(C(C(O3)COP(=O)([O-])OP(=O)([O-])OP(=O)([O-])O)O)O')
+A4P = Molecule.FromSmiles('C1=NC2=C(C(=N1)N)N=CN2C3C(C(C(O3)COP(=O)([O-])OP(=O)([O-])OP(=O)([O-])OP(=O)([O-])O)O)O')
 
 class GroupsDecompositionTest(unittest.TestCase):
     """Tests for GroupsDecomposition"""
@@ -17,39 +18,40 @@ class GroupsDecompositionTest(unittest.TestCase):
     
     def testFindPhosphateChains(self):
         ps = group_decomposition.GroupDecomposer.FindPhosphateChains(PHOSPHATE,
-                                                                     ignore_protonations=True)
+                                                                     ignore_protonations=False)
         
         for unused_grp, l in ps:
             self.assertTrue(not l)
         
         mk_ps_dict = lambda ps: dict((key, l) for key, l in ps)
+        mk_ps_string = lambda ps: ', '.join(["%s x %d" % (str(key), len(l)) for key, l in ps if l != []])
 
         # Find chains in A4P        
         ps = group_decomposition.GroupDecomposer.FindPhosphateChains(A4P,
-                                                                     ignore_protonations=True,
+                                                                     ignore_protonations=False,
                                                                      max_length=4)
+        logging.info("A4P: " + mk_ps_string(ps))
         ps_dict = mk_ps_dict(ps)
         
-        self.assertEqual(1, len(ps_dict[groups_data.GroupsData.MIDDLE_P_2]))
-        self.assertEqual(1, len(ps_dict[groups_data.GroupsData.MIDDLE_2_PHOSPHATE]))
-        self.assertEqual(1, len(ps_dict[groups_data.GroupsData.FINAL_P_1]))
-        ps_dict.pop(groups_data.GroupsData.MIDDLE_P_2)
-        ps_dict.pop(groups_data.GroupsData.MIDDLE_2_PHOSPHATE)
-        ps_dict.pop(groups_data.GroupsData.FINAL_P_1)
+        A4P_phosphate_groups = ['middle H0', 'middle chain H0', 'final H1']
+        for group_name in A4P_phosphate_groups:
+            l = ps_dict.pop(GroupsData.PHOSPHATE_DICT[group_name])
+            self.assertEqual(1, len(l))
         for l in ps_dict.itervalues():
             self.assertFalse(l)
         
         # Find chains in ATP
         ps = group_decomposition.GroupDecomposer.FindPhosphateChains(ATP,
-                                                                     ignore_protonations=True,
+                                                                     ignore_protonations=False,
                                                                      max_length=4)
+        logging.info("ATP: " + mk_ps_string(ps))
         ps_dict = mk_ps_dict(ps)
-        self.assertEqual(1, len(ps_dict[groups_data.GroupsData.MIDDLE_2_PHOSPHATE]))
-        self.assertEqual(1, len(ps_dict[groups_data.GroupsData.FINAL_P_1]))
-        ps_dict.pop(groups_data.GroupsData.MIDDLE_2_PHOSPHATE)
-        ps_dict.pop(groups_data.GroupsData.FINAL_P_1)
+        A4P_phosphate_groups = ['middle chain H0', 'final H1']
+        for group_name in A4P_phosphate_groups:
+            v = ps_dict.pop(GroupsData.PHOSPHATE_DICT[group_name])
+            self.assertEqual(1, len(v))
         for l in ps_dict.itervalues():
-            self.assertFalse(l)    
+            self.assertFalse(l)
     
     def testDecompose(self):
         # Phosphate has no decomposition.
@@ -71,6 +73,10 @@ class GroupsDecompositionTest(unittest.TestCase):
         pseudoisomers = decomposition.PseudoisomerVectors()
         self.assertFalse(pseudoisomers)
     
+def Suite():
+    suites = (unittest.makeSuite(GroupsDecompositionTest,'test'))
+    return unittest.TestSuite(suites)
+
     
 if __name__ == '__main__':
     unittest.main()
