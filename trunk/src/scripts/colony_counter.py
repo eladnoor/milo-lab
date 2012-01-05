3990997c636e717b13fcbf0d7ad49d81dde68e52
elad.noor@gmail.com <elad.noor@gmail.com@48c8c3c8-1ab2-6309-a68e-2c090a688e41>
2012-01-05 12:14:17 +0000

    more presice colony counting

--- trunk/src/scripts/colony_counter.py
+++ trunk/src/scripts/colony_counter.py
@@ -53,46 +53,100 @@ def DetectEdge(img, N=7):
     cv.Canny(img_g, out, lowThresh, highThresh, aperture_size=aperture_size)
     cv.ShowImage("edge", out)
 
+def GetCircle(center, radius):
+    x_points = []
+    y_points = []
+    for x in xrange(radius+1):
+        l = np.sqrt(2*x*radius - x**2)
+        r_y = range(int(np.floor(center[1]-l)), int(np.floor(center[1]+l)))
+        y_points += r_y
+        x_points += [int(np.floor(center[0] - radius + x))]*len(r_y)
+        if x < radius:
+            y_points += r_y
+            x_points += [int(np.floor(center[0] + radius - x))]*len(r_y)
+    return (x_points, y_points)
+
 def NumpyDetectEdge(fname, color_filter=(1, 1, 1), radius=510, center=(582, 791)):
+    plate_circle = GetCircle(center, radius)
+    
     im = misc.imread(fname)
     im_gray = np.dot(im, color_filter) / sum(color_filter)
-    imf = ndimage.gaussian_filter(im_gray, sigma=5)
-    imf = 256 - imf # invert color
-    
-    # cut out all except the circle containing the plate
-    for x in xrange(im.shape[0]):
-        l_sqr = radius**2 - (x-center[0])**2
-        if l_sqr < 0:
-            im[x, :, :] = 0
-        else:
-            l = np.sqrt(l_sqr)
-            imf[x, 0:(center[1]-l)] = 0
-            imf[x, (center[1]+l):] = 0
-    
-    T = 100
-    imf[np.where(imf < T)] = 0
-    
-    rmax = pymorph.regmax(imf)
-    seeds, n_colonies = ndimage.label(rmax) # gives a unique integer number to each region and fills it with that number
-    for i in xrange(n_colonies):
-        av = np.mean(imf[np.where(seeds==i)])
-        if av < T:
-            continue
-        x = np.mean(np.where(seeds==i)[0])
-        y = np.mean(np.where(seeds==i)[1])
-        im[x, y, :] = (255, 0, 0)
-    #im_gray = pymorph.overlay(im_gray, rmax)
-    plt.imshow(im)
+    #im_gray[plate_circle] = 0
     #plt.imshow(im_gray, plt.cm.gray)
-    plt.show()
+    #plt.show()
+    #return
+
+    imf = ndimage.gaussian_filter(im_gray, sigma=2)
+    imf = 256 - imf # invert color
+
+    imc = np.zeros(imf.shape, dtype=np.int32)
+    imc[plate_circle] = imf[plate_circle]
+    imc = imc[center[0]-radius:center[0]+radius, center[1]-radius:center[1]+radius]
+
+    T = 100
+    imc[np.where(imc < T)] = 0
+    
+    rmax = pymorph.regmax(imc)
+    seeds, n_colonies = ndimage.label(rmax) # gives a unique integer number to each region and fills it with that number
+    
+    count = 0
+    for i in xrange(n_colonies):
+        seed = np.where(seeds==i)
+        l = len(seed[0])
+        # filter seeds which are touching the perimeter of 
+        # the circle (false alarms)
+        perimeter = False
+        for j in xrange(l):
+            if np.sqrt((radius-seed[0][j])**2 + (radius-seed[1][j])**2) > radius-5:
+                perimeter = True
+        if perimeter:
+            continue
+
+        seedx = [(x + center[0] - radius) for x in seed[0]]
+        seedy = [(y + center[1] - radius) for y in seed[1]]
+        meanx = np.mean(seedx)
+        meany = np.mean(seedy)
+        
+        #av = np.mean(im[(seedx, seedy)])
+        #if av < T:
+        #    continue
+        
+        #for j in xrange(l):
+        #    im[seedx[j], seedy[j], :] = (255, 0, 0)
+        #print meanx, meany
+        colony_circle = GetCircle((meanx, meany), 3)
+        colony_color = np.min(imf[colony_circle])
+        if colony_color >= 110:
+            count += 1
+        if colony_color < 100:
+            im[meanx, meany, :] = (255, 0, 0) # red
+        elif 100 < colony_color < 110:
+            im[meanx, meany, :] = (255, 255, 0) # yellow
+        elif 110 < colony_color < 120:
+            im[meanx, meany, :] = (0, 255, 0) # green 
+        elif 120 < colony_color < 130:
+            im[meanx, meany, :] = (0, 255, 255) # cyan
+        elif 130 < colony_color < 140:
+            im[meanx, meany, :] = (0, 0, 255) # blue
+        elif 140 < colony_color < 150:
+            im[meanx, meany, :] = (255, 0, 255) # magenta
+        elif 150 < colony_color:
+            im[meanx, meany, :] = (255, 255, 255) # white
+        
+    #plt.imshow(im)
+    #plt.imshow(im_gray, plt.cm.gray)
+    #plt.show()
+    return count
 
 if __name__ == "__main__":
     fnames = []
-    fnames += ['/home/eladn/Dropbox/Experiments/ace-plates/2011-12-29 ace- 10^5.jpg']
-    fnames += ['/home/eladn/Dropbox/Experiments/ace-plates/2011-12-29 ace+ 10^5.jpg']
-    fnames += ['/home/eladn/Dropbox/Experiments/ace-plates/2012-01-03 ace- 10^5.jpg']
-    fnames += ['/home/eladn/Dropbox/Experiments/ace-plates/2012-01-03 ace+ 10^5.jpg']
-    NumpyDetectEdge(fnames[3], color_filter=(0, 0, 1)) # take only the blue color
+    #fnames += ['/home/eladn/Dropbox/Experiments/ace-plates/2011-12-29 ace- 10^5.jpg']
+    #fnames += ['/home/eladn/Dropbox/Experiments/ace-plates/2011-12-29 ace+ 10^5.jpg']
+    fnames += ['/home/eladn/Dropbox/Experiments/ace-plates/2012-01-03 ace- 10^3.jpg']
+    fnames += ['/home/eladn/Dropbox/Experiments/ace-plates/2012-01-03 ace+ 10^3.jpg']
+    
+    for fname in fnames:
+        print fname, NumpyDetectEdge(fname, color_filter=(0, 0, 1)) # take only the blue color
     #img = cv.LoadImage(fname)
     #DetectHough(img)
     #DetectEdge(img, 7)
