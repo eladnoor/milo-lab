c596901407c658dd7c61976843a09d375dbe6e87
elad.noor@gmail.com <elad.noor@gmail.com@48c8c3c8-1ab2-6309-a68e-2c090a688e41>
2012-01-19 16:03:24 +0000

    added more C1 pathways

--- trunk/src/scripts/odfe_vs_atp_carbon_fixation.py
+++ trunk/src/scripts/odfe_vs_atp_carbon_fixation.py
@@ -8,42 +8,16 @@ from pygibbs.pathway_modelling import KeggPathway, DeltaGNormalization
 from pygibbs.thermodynamic_estimators import LoadAllEstimators
 from pygibbs.metabolic_modelling.mtdf_optimizer import UnsolvableConvexProblemException
 from pygibbs.thermodynamic_constants import R
+from pygibbs.kegg_reaction import Reaction
 
-def pareto(html_writer, thermo,
+def pareto(html_writer, thermo, c_range=(1e-6, 1e-2),
+           cid2bounds=None,
            filename='../data/thermodynamics/odfe_vs_atp_carbon_fixation.txt'):
     
     entry2fields_map = ParsedKeggFile.FromKeggFile(filename)
     
     c_range = (1e-6, 1e-2)
-    cid2bounds = {
-                  1:   (1,    1),    # water
-                  11:  (1e-5, 1e-5), # CO2
-                  288: (9e-5, 9e-5), # carbonate
-                  2:   (5e-3, 5e-3), # ATP (in order to keep ATP -> ADP at -55 kJ/mol)
-                  8:   (5e-4, 5e-4), # ADP (in order to keep ATP -> ADP at -55 kJ/mol)
-                  9:   (5e-3, 5e-3), # Pi  (in order to keep ATP -> ADP at -55 kJ/mol)
-                  20:  (1e-4, 1e-4), # AMP (in order to keep ATP -> AMP at -110 kJ/mol)
-                  13:  (3e-9, 3e-9), # PPi (in order to keep ATP -> AMP at -110 kJ/mol)
-                  #20:  (5e-3, 5e-3), # AMP (in order to keep ATP -> AMP at -55 kJ/mol)
-                  #13:  (5e-2, 5e-2), # PPi (in order to keep ATP -> AMP at -55 kJ/mol)
-                  3:   (1e-3, 1e-3), # NAD_ox
-                  4:   (1e-4, 1e-2), # NAD_red (to keep ox/red between 0.1 and 10)
-                  6:   (1e-3, 1e-3), # NADP_ox
-                  5:   (1e-4, 1e-2), # NADP_red (to keep ox/red between 0.1 and 10)
-                  399: (1e-3, 1e-3), # ubiquinone_ox
-                  390: (1e-4, 1e-2), # ubiquinol_red (to keep ox/red between 0.1 and 10)
-                  139: (1e-3, 1e-3), # ferredoxin_ox
-                  138: (1e-4, 1e-2), # ferredoxin_red (to keep ox/red between 0.1 and 10)
-
-                  #3:   (5e-3, 5e-3), # NAD_ox
-                  #4:   (5e-5, 5e-5), # NAD_red (to keep ox/red at 100)
-                  #6:   (5e-5, 5e-5), # NADP_ox
-                  #5:   (5e-4, 5e-4), # NADP_red (to keep ox/red at 0.1)
-                  #399: (1e-3, 1e-3), # ubiquinone_ox
-                  #390: (1e-4, 1e-2), # ubiquinol_red (to keep ox/red between 0.1 and 10)
-                  #139: (1e-6, 1e-6), # ferredoxin_ox
-                  #138: (1e-2, 1e-2), # ferredoxin_red (to keep ox/red at 1e-4)
-                  }
+    cid2bounds = cid2bounds or {}
     
     keys = sorted(entry2fields_map.keys())
     plot_data = np.zeros((len(keys), 4))
@@ -85,31 +59,68 @@ def pareto(html_writer, thermo,
             
         plot_data[i, :] = [mtdf, odfe, total_dG_prime, np.sum(fluxes)]
     
-    fig = plt.figure(figsize=(6, 6), dpi=80)
+    fig = plt.figure(figsize=(6, 6), dpi=60)
     plt.plot(plot_data[:, 0], plot_data[:, 2], '.', figure=fig)
     for i, key in enumerate(keys):
         if plot_data[i, 0] < 0:
-            color = 'red'
+            color = 'grey'
         else:
             color = 'black'
         plt.text(plot_data[i, 0], plot_data[i, 2]+3, key, ha='center', va='bottom', fontsize=10, color=color)
     plt.xlabel('Optimized Distributed Bottleneck [kJ/mol]', figure=fig)
-    plt.ylabel('Optimal Energetic Cost [kJ/mol]', figure=fig)
+    plt.ylabel('Optimal Energetic Efficiency [kJ/mol]', figure=fig)
     return fig
     
 if __name__ == "__main__":
     thermo = LoadAllEstimators()['merged_C1']
     output_filename = '../res/odfe_vs_atp_carbon_fixation.html'
     html_writer = HtmlWriter(output_filename)
-    for pH in [5, 7]:
-        thermo.SetConditions(pH=pH, I=0.1, T=298.15, pMg=10)
+    cid2bounds = {
+                  1:   (1,    1),    # water
+                  #11:  (1e-5, 1e-5), # CO2
+                  #288: (9e-5, 9e-5), # carbonate
+                  2:   (5e-3, 5e-3), # ATP (in order to keep ATP -> ADP at -55 kJ/mol)
+                  8:   (5e-4, 5e-4), # ADP (in order to keep ATP -> ADP at -55 kJ/mol)
+                  9:   (5e-3, 5e-3), # Pi  (in order to keep ATP -> ADP at -55 kJ/mol)
+                  20:  (1e-4, 1e-4), # AMP (in order to keep ATP -> AMP at -110 kJ/mol)
+                  13:  (3e-9, 3e-9), # PPi (in order to keep ATP -> AMP at -110 kJ/mol)
+                  #20:  (5e-3, 5e-3), # AMP (in order to keep ATP -> AMP at -55 kJ/mol)
+                  #13:  (5e-2, 5e-2), # PPi (in order to keep ATP -> AMP at -55 kJ/mol)
+                  3:   (1e-3, 1e-3), # NAD_ox
+                  4:   (1e-4, 1e-2), # NAD_red (to keep ox/red between 0.1 and 10)
+                  6:   (1e-3, 1e-3), # NADP_ox
+                  5:   (1e-4, 1e-2), # NADP_red (to keep ox/red between 0.1 and 10)
+                  399: (1e-3, 1e-3), # ubiquinone_ox
+                  390: (1e-4, 1e-2), # ubiquinol_red (to keep ox/red between 0.1 and 10)
+                  139: (1e-3, 1e-3), # ferredoxin_ox
+                  138: (1e-4, 1e-2), # ferredoxin_red (to keep ox/red between 0.1 and 10)
 
-        #html_writer.write('<h1>pH = %g</h1>\n' % pH)
-        html_writer.insert_toggle(start_here=True)
-        fig = pareto(html_writer, thermo)
-        html_writer.div_end()
-        
-        plt.title('pH = %g' % pH, figure=fig)
-        html_writer.embed_matplotlib_figure(fig)
+                  #3:   (5e-3, 5e-3), # NAD_ox
+                  #4:   (5e-5, 5e-5), # NAD_red (to keep ox/red at 100)
+                  #6:   (5e-5, 5e-5), # NADP_ox
+                  #5:   (5e-4, 5e-4), # NADP_red (to keep ox/red at 0.1)
+                  #399: (1e-3, 1e-3), # ubiquinone_ox
+                  #390: (1e-4, 1e-2), # ubiquinol_red (to keep ox/red between 0.1 and 10)
+                  #139: (1e-6, 1e-6), # ferredoxin_ox
+                  #138: (1e-2, 1e-2), # ferredoxin_red (to keep ox/red at 1e-4)
+                  }
+    co2_hydration = Reaction.FromFormula("C00011 + C00001 => C00288")
+    
+    I, T, pMg = 0.1, 298.15, 10
+    for pH in [5, 7]:
+        thermo.SetConditions(pH=pH, I=I, T=T, pMg=pMg)
+        co2_hydration_dG0_prime = thermo.GetTransfromedKeggReactionEnergies([co2_hydration])[0, 0]
+        for co2_conc in [1e-5, 1e-3]:
+            carbonate_conc = co2_conc * np.exp(-co2_hydration_dG0_prime / (R*T))
+            cid2bounds[11] = (co2_conc, co2_conc)
+            cid2bounds[288] = (carbonate_conc, carbonate_conc)
+            
+            #html_writer.write('<h1>pH = %g</h1>\n' % pH)
+            html_writer.insert_toggle(start_here=True)
+            fig = pareto(html_writer, thermo, cid2bounds=cid2bounds)
+            html_writer.div_end()
+            
+            plt.title('pH = %g, [CO2] = %g mM' % (pH, co2_conc*1000), figure=fig)
+            html_writer.embed_matplotlib_figure(fig)
     
     html_writer.close()
