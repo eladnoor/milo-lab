c795a20a51b8d92cf68300281e446cc94eeb31d1
elad.noor@gmail.com <elad.noor@gmail.com@48c8c3c8-1ab2-6309-a68e-2c090a688e41>
2013-01-15 16:52:25 +0000

    tweaks to the Victor uploader

--- src/pyrobot/web/cgi-bin/victor_uploader.py
+++ src/pyrobot/web/cgi-bin/victor_uploader.py
@@ -1,63 +1,40 @@
 from argparse import ArgumentParser
-import sys, os
+import time, sys, os
 from database import MySQLDatabase, SqliteDatabase
-import time
 from victor_parser import VictorParser
-
-# all Victor experiments have only one plate, so their ID is 0
-PLATE_ID = 0
 
 def MakeOpts():
     """Returns an OptionParser object with all the default options."""
     parser = ArgumentParser()
     parser.add_argument('-d', '--debug', action='store_true',
                         help='debug mode, store results in dummy DB')
+    parser.add_argument("-o", "--host", dest="host", default="hldbv02",
+                        help="The hostname for the MySQL database")
     parser.add_argument('-e', '--exp_id', default=None,
                         help='Override the experiment ID '
                              '(default is to use the measurement date-time)')
-    parser.add_argument('xls_file',
-                        help='The path to the XLS file containing Victor results')
+    parser.add_argument('xls_file', help='The path to the XLS file containing Victor results')
     return parser
 
-def GetTimeString():
-    return time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
-
-def GetLatestFile(path):
-    filelist = [f for f in os.listdir(path) if f[-4:] == '.xls']
-    filelist = filter(lambda x: not os.path.isdir(path + x), filelist)
-    return max(filelist, key=lambda x: os.stat(path + x).st_mtime)
-
 def CreateDummyDB():
-    db = SqliteDatabase('/tmp/victor_dummy.sqlite', 'w')
-    db.CreateTable('tecan_readings', 'exp_id TEXT, plate_id TEXT, '
-                   'measurement_name TEXT, row INT, col INT, time_in_sec INT, '
-                   'value REAL', drop_if_exists=False)
-    db.CreateTable('tecan_experiments', 'exp_id TEXT, serial_number TEXT, desc TEXT',
+    db = SqliteDatabase('/tmp/dummy.sqlite', 'w')
+    db.CreateTable('tecan_readings',
+                   'exp_id TEXT, plate TEXT, reading_label TEXT, row INT, col INT, time INT, measurement REAL',
                    drop_if_exists=False)
-    db.CreateTable('tecan_plates', 'exp_id TEXT, plate_id TEXT, '
-                   'field1 TEXT, field2 INT, field3 INT', drop_if_exists=False)
+    db.CreateTable('tecan_labels',
+                   'exp_id TEXT, plate INT, row INT, col INT, label TEXT',
+                   drop_if_exists=False)
+    db.CreateTable('tecan_plates',
+                   'exp_id TEXT, plate INT, description TEXT, owner TEXT, project TEXT',
+                   drop_if_exists=False)
+    db.CreateTable('tecan_experiments',
+                   'exp_id TEXT, serial_number TEXT, desciption TEXT',
+                   drop_if_exists=False)
+    db.CreateTable('tecan_scripts',
+                   'exp_id TEXT, script BLOB',
+                   drop_if_exists=False)
     return db
 
-def ImportFileToDatabase(fp, db, exp_id=None):
-    vp = VictorParser()
-    vp.parse_excel(fp)
-    
-    exp_id = exp_id or vp.get_time_string()   
-    print "Experiment ID: " + exp_id
-
-    # delete any previous data regarding this exp_id
-    db.Execute("DELETE FROM tecan_readings WHERE exp_id='%s'" % exp_id)
-    db.Execute("DELETE FROM tecan_experiments WHERE exp_id='%s'" % exp_id)
-    db.Execute("DELETE FROM tecan_plates WHERE exp_id='%s'" % exp_id)
-    
-    desc = "Imported from Victor on " + GetTimeString()
-    db.Insert('tecan_experiments', [exp_id, desc])
-    db.Insert('tecan_plates', [exp_id, PLATE_ID, "", None, None])
-    vp.write_to_database(db, exp_id)
-    db.Commit()
-    
-    return exp_id
-    
 def main():
     """
         Imports an XLS file of Victor exported results.
@@ -69,24 +46,16 @@ def main():
     if options.debug:
         db = CreateDummyDB()
     else:
-        db = MySQLDatabase(host='hldbv02', user='ronm', port=3306,
+        db = MySQLDatabase(host=options.host, user='ronm', port=3306,
                            passwd='a1a1a1', db='tecan')
 
-    if options.xls_file is not None:
-        xls_filename = options.xls_file
-    else:
-        if not os.path.exists(options.xls_dir):
-            print "Directory not found: " + options.xls_dir
-            sys.exit(-1)
-        xls_filename = options.xls_dir + GetLatestFile(options.xls_dir)
-        
-    if not os.path.exists(xls_filename):
-        print "File not found: " + xls_filename
+    if not os.path.exists(options.xls_file):
+        print "File not found: " + options.xls_file
         sys.exit(-1)
     
-    print "Importing from file: " + xls_filename
-    fp = open(xls_filename, 'r')
-    exp_id = ImportFileToDatabase(fp, db, options.exp_id)
+    print "Importing from file: " + options.xls_file
+    fp = open(options.xls_file, 'r')
+    exp_id = VictorParser.ImportFileToDB(fp, db, options.exp_id)
     if options.debug:
         print "Done, go check out the results at %s" % db.filename
     else:
