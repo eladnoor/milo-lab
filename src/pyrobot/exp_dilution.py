847a9259f776efffe699e47719f1b58d4255b5a5
niv.anto@gmail.com <niv.anto@gmail.com@48c8c3c8-1ab2-6309-a68e-2c090a688e41>
2013-04-07 20:06:58 +0000

    Added Header() and Footer() functions for opening and closing worklist commands besides pipetation.

--- src/pyrobot/exp_dilution.py
+++ src/pyrobot/exp_dilution.py
@@ -4,16 +4,29 @@ import sys
 from database import MySQLDatabase
 import numpy as np
 
-def SetFile(filename):
+def Header():
     """
     Creates a worklist file and writes the first init command (tip size to be used in the fllowing commands)
     """
-    f = open(filename, 'w')
-    f.write('S;1')
-    f.close()
-    return 1
+    header = [] 
+    header += ['S;1']
+    header +=  ['Vector("Liconic","69","1",0,1,0,1,0,0);']
+    header +=  ['Vector("Grid40","40","3",0,1,2,0,0,0);']
     
-def Comm(x,labware,row,col,vol,liq,filename):
+    return '\n'.join(header)
+    
+def Footer():
+    """
+    Creates a worklist file and writes the first init command (tip size to be used in the fllowing commands)
+    """
+    footer = []
+    footer += ['Vector("Grid40","40","3",0,0,0,1,0,0);']
+    footer +=  ['Vector("Liconic","69","1",0,1,2,0,0,0);']
+     
+    return '\n'.join(footer)
+
+    
+def Comm(x,labware,row,col,vol,liq):
     """
     Append a command into the worklist file
     Params --> x='A' for aspiarte or x='D' for dispense (char)
@@ -22,32 +35,21 @@ def Comm(x,labware,row,col,vol,liq,filename):
                vol = volume in ul  (int)
                liq = liquid class (string)
     """
-    f = open(wl,'a')
-    pos = (col-1)*8+row
-    f.write("\n%s;%s;;;%s;;%s;%s;;;" % (x,labware,pos,vol,liq))
-    #print "%s;%s;;;%s;;%s;%s;;;" % (comm,loc,pos,vol,liq)
-    f.close()
+    pos = (col)*8+row + 1
+    return '%s;%s;;;%d;;%d;%s;;;' % (x,labware,pos,vol,liq)
     
-def Tip(filename):
+def Tip():
     """
     Append a change tup command to worklist file
     """
-    f = open(wl,'a')
-    f.write("\nW;") 
-    #print "W;"
-    f.close()
+    return 'W;'
     
-def UserPrompt(msg,filename):
+def UserPrompt(msg):
     """
     Append a UserPrompt evoke command to the worklist file
     text will be displayed to evoware user while executing this worklist command
     """
-    f = open(wl,'a')
-    f.write('\nB;UserPrompt(%s,0,-1);' % (msg))
-    print '%s' % command
-    f.close()  
-
-
+    return 'B;UserPrompt("%s",1,-5);' % (msg)
 
 def MakeOpts():
     """Returns an OptionParser object with all the default options."""
@@ -59,9 +61,10 @@ def MakeOpts():
                         help='debug mode, store results in dummy DB')
     parser.add_argument('-w', '--worklist', dest='worklist',
                         help='the path to the worklist that will be written')
-    parser.add_argument('-t', '--threshold', dest='threshold', default=0.5,
+    parser.add_argument('-t', '--threshold', dest='threshold', default=0.2, type=float,
                         help='the OD threshold for dilution')
-    
+    parser.add_argument('-v', '--volume', dest='vol', default=15, type=int,
+                        help='volume for diluation in ul')
     return parser
 
 def get_last_plate(db):
@@ -77,7 +80,7 @@ def get_last_plate(db):
     print "Exp ID: %s, Plate: %d, Time: %d" % (exp_id, plate, max_time)
     return exp_id, plate, max_time
 
-def get_dilution_rows(db, exp_id, plate):
+def get_dilution_rows(db, exp_id, plate, time):
     """
         returns a vector (length 12) of the current rows that should be
         checked for dilution. If they don't exist returns zeros.
@@ -86,39 +89,38 @@ def get_dilution_rows(db, exp_id, plate):
     res = db.Execute('SELECT COUNT(*) FROM exp_dilution_columns WHERE exp_id="%s" AND plate=%d'%  (exp_id, plate))
     if res[0][0] == 0:
         for col in xrange(12):
-            db.Execute('INSERT INTO exp_dilution_columns(exp_id, plate, col, row) VALUES ("%s", %d, %d, 0)' % (exp_id, plate, col))
+            db.Execute('INSERT INTO exp_dilution_columns(exp_id, plate, col, row, time) VALUES ("%s", %d, %d, 0, %d)' %
+                       (exp_id, plate, col, time))
 
     dilution_rows = [0] * 12
    
     # if the exp_id doesn't exist this loop will not be skipped and the result
     # will be only 0s
     print 'SELECT col, row FROM exp_dilution_columns WHERE exp_id="%s" AND plate=%d' % (exp_id, plate)
-    for res in db.Execute('SELECT col, row FROM exp_dilution_columns WHERE exp_id="%s" AND plate=%d'
+    for res in db.Execute('SELECT col, max(row) FROM exp_dilution_columns WHERE exp_id="%s" AND plate=%d GROUP BY col'
                           % (exp_id, plate)):
         col, row = res
         dilution_rows[col] = row
     return dilution_rows
 
-def write_dilution_rows(db, exp_id, plate, dilution_rows):
-    for col, row in enumerate(dilution_rows):
-        db.Execute('UPDATE exp_dilution_columns SET row=%d WHERE (exp_id="%s" AND plate=%d AND col=%d)' 
-                   % (row, exp_id, plate, col))
+def increment_row(db, exp_id, plate, col, row, time):
+    db.Execute('INSERT INTO exp_dilution_columns(exp_id, plate, col, row, time)  VALUES ("%s", %d, %d, %d, %d)' %
+               (exp_id, plate, col, row, time))
 
 def main():
+
     options = MakeOpts().parse_args()
+    VOL = options.vol
     LABWARE = 'LABWARE' 
-    VOL = 15
     LIQ = 'LIQUID_CLASS'
     
-    filename = options.worklist
     # We should also state which directory where the evoware could find the worklist file
-    SetFile(filename)
 
     db = MySQLDatabase(host=options.host, user='ronm', port=3306,
                        passwd='a1a1a1', db='tecan')
     
     exp_id, plate, max_time = get_last_plate(db)
-    dilution_rows = get_dilution_rows(db, exp_id, plate)
+    dilution_rows = get_dilution_rows(db, exp_id, plate, max_time)
     
     print "dilution_rows: ", dilution_rows
     
@@ -127,21 +129,26 @@ def main():
         row, col, measurement = res
         data[row, col] = measurement
 
+    worklist = []
     for col, row in enumerate(dilution_rows):
         meas = data[row, col]
         print col, row, meas
-        if meas > options.threshold:
-            msg = "OD = %d --> dilute cell (%d, %d) into cell (%d, %d)" % (meas, row, col, row+1, col)
+        if (meas > options.threshold) and (row < 7):
+            msg = "OD = %f --> dilute cell %s%d into cell %s%d" % (meas, chr(ord('A') + row), col+1, chr(ord('A') + row + 1), col+1)
             print msg
-            UserPrompt(msg,filename)
-            Comm(A,LABWARE,row,col,VOL,LIQ,filename)
-            Comm(D,LABWARE,row+1,col,VOL,LIQ,filename)
+            worklist += [UserPrompt(msg)]
+            worklist += [Comm('A',LABWARE,row,col,VOL,LIQ)]
+            worklist += [Comm('D',LABWARE,row+1,col,VOL,LIQ)]
             #labware,volume and liquid_class would be hard coded for now ...
-            Tip(filename)
-            dilution_rows[col] += 1
-    
-    write_dilution_rows(db, exp_id, plate, dilution_rows)
-    
+            worklist += [Tip()]
+            increment_row(db, exp_id, plate, col, row+1, max_time)
+        
+    if len(worklist) > 0:
+        worklist = [Header()] + worklist + [Footer()]
+
+    f = open(options.worklist, 'w')
+    f.write('\n'.join(worklist))
+    f.close()
     db.Commit()
     print "Done!"
     sys.exit(0)
