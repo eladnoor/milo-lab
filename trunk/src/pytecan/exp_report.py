56aa610aa241879447410286c2c1a0afc6d085b2
niv.anto@gmail.com <niv.anto@gmail.com@48c8c3c8-1ab2-6309-a68e-2c090a688e41>
2013-05-27 11:42:24 +0000

    Updated exp_report to include the robotsites html page as an attachment in the mail

--- trunk/src/pytecan/exp_report.py
+++ trunk/src/pytecan/exp_report.py
@@ -3,15 +3,27 @@ import numpy as np
 from argparse import ArgumentParser
 from toolbox.database import MySQLDatabase
 import smtplib
+import urllib2
+
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEBase import MIMEBase
+from email.MIMEText import MIMEText
+from email import Encoders
  
 SMTP_SERVER = 'smtp.gmail.com'
 SMTP_PORT = 587
-
-password = 'alphadelta'
- 
-sender = 'niv.anto@gmail.com'
+password = 'hamshira6'
+sender = 'milolab9@gmail.com'
 recipient = 'niv.anto.projects@gmail.com'
-subject = 'Test'
+subject = ''
+
+
+
+
+
+
+
+
 
  
 
@@ -62,23 +74,47 @@ def main():
                        passwd='a1a1a1', db='tecan')
     
     plate_id = options.iteration % options.num_plates
-
     exp_id, max_time = GetLastPlate(db, plate_id, options.reading_label)
+    exp_id_url = exp_id.replace(" ","%20")
+    
+    url = 'http://eladpc1/RoboSite/graph/%s/%s/OD600/Read' % (exp_id_url,plate_id)
+    response = urllib2.urlopen(url)
+    print "Response:", response
+    # Get the URL. This gets the real URL. 
+    print "The URL is: ", response.geturl()   
+    # Get all data
+    html = response.read().replace('/js/highcharts.js','http://code.highcharts.com/highcharts.js').replace('/js/modules/exporting.js','http://code.highcharts.com/modules/exporting.js')
+    print "Get all data: ", html
+    file = 'attach.html'
+    fh = open(file, "w")
+    fh.write(html)
+    fh.close()
+    
+    
+   
+    
+    
+    
+    
+
+    
+    
+    
     subject = "%s --> plate : %d " % (exp_id,plate_id)
     data = GetMeasuredData(db, exp_id, max_time, plate_id, options.reading_label)
     
     csv ='' 
     string=''
     for c in range(12):  
-        string += "<br>"
+        string += "<BR>"
         for r in range(8):
             #string += data[r,c].astype('|S6')
             string += "%d , %d --> %s"  % (r+1,c+1,data[r,c].astype('|S6'))
-            string += '<br>' 
+            string += '<BR>' 
             print "%d , %d --> %f"  % (r+1,c+1,data[r,c]) 
 
     for r in range(8):  
-        csv += "<br>"
+        csv += "<BR>"
         for c in range(12):
             csv += data[r,c].astype('|S6')
             if (c < 11) :
@@ -88,14 +124,33 @@ def main():
             
             
 
-    #f = open(options.worklist[0], 'w')
-    #f.write(csv)
-    #f.close() 
     
     print string
     body = string
-    body += '<br> <br>'
+    body += '<BR><BR>'
     body += csv
+    msg = MIMEMultipart()
+    msg['Subject'] = subject
+    msg['From'] = sender
+    content = MIMEText(body,'html')
+    msg.attach(content)
+    
+    part = MIMEBase('application', 'octet-stream')
+    part.set_payload(open(file, 'rb').read())
+    Encoders.encode_base64(part)
+    exp = exp_id+'.html'
+    part.add_header('Content-Disposition', 'attachment; filename="%s"' % exp)
+    msg.attach(part)
+    session = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
+    session.ehlo()
+    session.starttls()
+    session.login(sender, password)
+    session.sendmail(sender, recipient, msg.as_string())
+    
+    
+    sys.exit(1)
+    
+    
     headers = ["From: " + sender,
     "Subject: " + subject,
     "To: " + recipient,
@@ -111,6 +166,7 @@ def main():
     session.quit()
     
     
+  
     
     
     print "Done!"
